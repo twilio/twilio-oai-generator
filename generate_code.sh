1e5b1717a16280dd37f2587731638ad85645394f
Shwetha Radhakrishna <shwetha.manvinkurke@gmail.com>
2021-01-13 17:36:33 -0700

    Modified generate code

--- generate_code.sh
+++ generate_code.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-for domain in ~/DI/apis/*.json; do
+for domain in ~/DI/apis/*.yaml; do
   fullname="$(basename "$domain")"
   filename="${fullname%.*}"
   domain_name="$(cut -d'_' -f2 <<< "$filename")"
   version="$(cut -d'_' -f3 <<< "$filename")"
 #  echo "$domain_name"
-  java -cp ~/packages/openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar org.openapitools.codegen.OpenAPIGenerator generate -g twilio-go -i "$domain" -o ./twilio-go-json/"$domain_name"/"$version"
+  java -cp ~/packages/openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar org.openapitools.codegen.OpenAPIGenerator generate -g twilio-go -i "$domain" -o ./twilio-go/"$domain_name"/"$version"
 done
 
