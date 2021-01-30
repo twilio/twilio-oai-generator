ba75b953d2f5c0d64df6d24082cbc7c8ebba6b5d
Elmer Thomas <ethomas@twilio.com>
2021-01-29 17:02:45 -0800

    path update

--- examples/build-twilio-go.sh
+++ examples/build-twilio-go.sh
@@ -4,5 +4,5 @@ for domain in <path-to>/twilio-oai/spec/yaml/twilio_*.yaml; do
   filename="${fullname%.*}"
   domain_name="$(cut -d'_' -f2 <<< "$filename")"
   version="$(cut -d'_' -f3 <<< "$filename")"
-  java -cp <path-to>/openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar org.openapitools.codegen.OpenAPIGenerator generate -g twilio-go -i "$domain" -o ./twilio-go/"$domain_name"/"$version" --ignore-file-override=./.openapi-generator
+  java -cp <path-to>/openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar org.openapitools.codegen.OpenAPIGenerator generate -g twilio-go -i "$domain" -o <path-to>/twilio-go/"$domain_name"/"$version" --ignore-file-override=./.openapi-generator
 done
