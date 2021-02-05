5a1700f8bd6e08a6faac89fd575aad72205f7f76
Shwetha Radhakrishna <shwetha-manvinkurke@users.noreply.github.com>
2021-02-04 19:24:22 -0700

    Updated example gen (#6)
    * Updated example gen
    
    * fix path

---
+++ examples/build_twilio_go.py
@@ -0,0 +1,21 @@
+import os
+import re
+
+regex = ".+?(?=_v[0-9]+)"
+
+openapi_spec_path = 'path/to/apis/yaml'
+
+for domain in os.listdir(openapi_spec_path):
+    full_path = os.path.join(openapi_spec_path, domain)
+    fullname = os.path.splitext(domain)[0]
+
+    regex_splits = re.split(r'(.+?(?=_v[0-9]+))', fullname, flags=re.IGNORECASE)
+    splits = list(filter(None, regex_splits))
+
+    domain_name = splits[0].replace('twilio_', "")
+    version = (splits[1]).replace('_', '') if len(splits) > 1 else ''
+
+    command = f"java -cp ./openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar " \
+              f"org.openapitools.codegen.OpenAPIGenerator generate -g twilio-go -i {full_path} -o " \
+              f"path/to/twilio-go/rest/{domain_name}/{version} --ignore-file-override=./.openapi-generator "
+    os.system(command)
