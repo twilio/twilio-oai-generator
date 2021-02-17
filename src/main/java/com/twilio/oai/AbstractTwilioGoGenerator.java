e588589bc6214daca61eb1df78f9f168d13036ab
Elise Shanholtz <eshanholtz@twilio.com>
2021-02-17 14:36:13 -0800

    Revert "fix: filter out duplicate enum values (#7)"
    This reverts commit 106fecb129dbd66cf3fa01bb04c1f58b43381400.

--- src/main/java/com/twilio/oai/AbstractTwilioGoGenerator.java
+++ src/main/java/com/twilio/oai/AbstractTwilioGoGenerator.java
@@ -8,7 +8,9 @@ import org.openapitools.codegen.CodegenParameter;
 import static org.openapitools.codegen.utils.StringUtils.camelize;
 import io.swagger.v3.oas.models.media.Schema;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class AbstractTwilioGoGenerator extends GoClientCodegen {
 
@@ -23,7 +25,6 @@ public abstract class AbstractTwilioGoGenerator extends GoClientCodegen {
 		super.processOpts();
 
 		additionalProperties.put(CodegenConstants.IS_GO_SUBMODULE, true);
-		additionalProperties.put(CodegenConstants.ENUM_CLASS_PREFIX, true);
 
 		supportingFiles.clear();
 	}
@@ -59,23 +60,5 @@ public abstract class AbstractTwilioGoGenerator extends GoClientCodegen {
 		name = name.replace(">","After");
 		name = super.toVarName(name);
 		return name;
-	}
-
-	@Override
-	protected List<Map<String, Object>> buildEnumVars(List<Object> values, String dataType) {
-		List<Map<String, Object>> siftedEnumVars = new ArrayList<>();
-		Set<Object> uniqueNames = new HashSet<>();
-		List<Map<String, Object>> rawEnumVars = super.buildEnumVars(values, dataType);
-
-		// Filter out duplicate enum values. Example: HTTPMETHOD_HEAD = "head"/HTTPMETHOD_HEAD = "HEAD"
-		for (Map<String, Object> enumVar : rawEnumVars) {
-			Object enumName = enumVar.get("name");
-			if (!uniqueNames.contains(enumName)) {
-				uniqueNames.add(enumName);
-				siftedEnumVars.add(enumVar);
-			}
-		}
-
-		return siftedEnumVars;
 	}
 }
