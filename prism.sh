f789f52ecb42faf26ee915e2aa1e41e0cd478a81
Elise Shanholtz <eshanholtz@twilio.com>
2021-03-30 16:29:10 -0700

    readability

--- prism.sh
+++ prism.sh
@@ -3,7 +3,11 @@ set -e
 
 rm -rf codegen && mkdir -p codegen
 make install
-java -cp ./openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar org.openapitools.codegen.OpenAPIGenerator generate -g twilio-go -i examples/go/oai.yaml -o codegen/twilio/rest/oai/
+java -cp ./openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar \
+  org.openapitools.codegen.OpenAPIGenerator \
+  generate -g twilio-go \
+  -i examples/go/oai.yaml \
+  -o codegen/twilio/rest/oai/
 cd examples/go/prism
 docker-compose build
 docker-compose up --force-recreate --abort-on-container-exit
