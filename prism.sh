3db9425fb6626fb62c52736f700a6b19a546e720
Elise Shanholtz <eshanholtz@twilio.com>
2021-03-31 09:55:46 -0700

    chore: update Travis build (#23)

--- prism.sh
+++ prism.sh
@@ -3,7 +3,11 @@ set -e
 
 rm -rf codegen && mkdir -p codegen
 make install
-python3 examples/build_twilio_go.py examples/go/oai.yaml codegen
+java -cp ./openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar \
+  org.openapitools.codegen.OpenAPIGenerator \
+  generate -g twilio-go \
+  -i examples/go/oai.yaml \
+  -o codegen/twilio/rest/oai/
 cd examples/go/prism
 docker-compose build
 docker-compose up --force-recreate --abort-on-container-exit
