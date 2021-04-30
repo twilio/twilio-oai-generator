a8909ff11b4b320c2eab09d33e97c07e33ddaf0f
Jennifer Mah <jennifermah98@gmail.com>
2021-04-30 12:55:16 -0600

    use prism.sh file

--- prism.sh
+++ prism.sh
@@ -8,6 +8,7 @@ java -cp ./openapi-generator-cli.jar:target/twilio-openapi-generator-1.0.0.jar \
   generate -g twilio-go \
   -i examples/go/oai.yaml \
   -o codegen/twilio/rest/oai/
+cp -R codegen/ examples/go
 cd examples/go/prism
 docker-compose build
 docker-compose up --force-recreate --abort-on-container-exit --remove-orphans
