918c03c44b1cacc2522e627e9ed7dc855c067ab2
Jennifer Mah <jennifermah98@gmail.com>
2021-05-03 17:40:30 -0600

    move generated files

--- prism.sh
+++ prism.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-rm -rf codegen && mkdir -p codegen
+cd examples/go && rm -rf twilio && mkdir -p twilio && cd ../../
 make install
 java -cp ./openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar \
   org.openapitools.codegen.OpenAPIGenerator \
   generate -g twilio-go \
   -i examples/go/oai.yaml \
-  -o codegen/twilio/rest/oai/
+  -o examples/go/twilio/rest/oai/
 cd examples/go/prism
 docker-compose build
 docker-compose up --force-recreate --abort-on-container-exit --remove-orphans
