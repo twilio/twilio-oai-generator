c4fb51ca7d0c5dedf49845cbf702f0cc89bc1434
Jennifer Mah <jennifermah98@gmail.com>
2021-05-03 14:55:54 -0600

    prune docker

--- prism.sh
+++ prism.sh
@@ -2,6 +2,7 @@
 set -e
 
 rm -rf codegen && mkdir -p codegen
+docker system prune -af
 make install
 java -cp ./openapi-generator-cli.jar:target/twilio-go-openapi-generator-1.0.0.jar \
   org.openapitools.codegen.OpenAPIGenerator \
