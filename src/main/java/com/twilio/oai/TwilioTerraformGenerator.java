293b770c387c904f451da58326a65160f45c0b98
Elmer Thomas <ethomas@twilio.com>
2021-06-07 17:04:14 -0700

    feat: no read only resources in the provider

--- src/main/java/com/twilio/oai/TwilioTerraformGenerator.java
+++ src/main/java/com/twilio/oai/TwilioTerraformGenerator.java
@@ -118,8 +118,8 @@ public class TwilioTerraformGenerator extends AbstractTwilioGoGenerator {
         if (operationName.startsWith("Delete")) {
             resource.put("hasDelete", true);
         }
-
         resource.put("hasAllCrudOps", (Boolean) resource.getOrDefault("hasCreate", false) && (Boolean) resource.getOrDefault("hasRead", false) && (Boolean) resource.getOrDefault("hasUpdate", false) && (Boolean) resource.getOrDefault("hasDelete", false));
+        resource.put("readOnly", (!((Boolean) resource.getOrDefault("hasCreate", false)) && !((Boolean) resource.getOrDefault("hasUpdate", false)) && !((Boolean) resource.getOrDefault("hasDelete", false))));
     }
 
     private ArrayList<Object> getResponseProperties(String coPath, String statusCode, Set<String> requestParams) {
