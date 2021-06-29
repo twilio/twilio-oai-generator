b37c798a21b1967f17b2c16c06eebfafcafeb944
childish-sambino <sharrison@twilio.com>
2021-06-29 17:01:56 -0500

    test: refactor go tests to prep for terraform tests (#76)

--- prism.sh
+++ prism.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 set -e
 
-REST_DIR=examples/go/rest/oai
-
-rm -rf $REST_DIR
 make install
+
+API_SPEC=examples/twilio_api_v2010.yaml
+
+OUT_DIR=examples/go/rest
+rm -rf $OUT_DIR
 java -cp ./openapi-generator-cli.jar:target/twilio-openapi-generator.jar \
   org.openapitools.codegen.OpenAPIGenerator \
   generate -g twilio-go \
-  -i examples/oai.yaml \
-  -o $REST_DIR
-cd examples/go/prism
+  -i $API_SPEC \
+  -o $OUT_DIR/api/v2010
+
+cd examples/prism
 docker-compose build
 docker-compose up --force-recreate --abort-on-container-exit --remove-orphans
