2ecd2b8204545341ac19d28bcf6daf4ab6f5076a
Elise Shanholtz <eshanholtz@twilio.com>
2021-10-06 10:41:09 -0700

    fix: add computed terraform resource properties (#90)

--- examples/terraform/unit/credentials_aws_test.go
+++ examples/terraform/unit/credentials_aws_test.go
@@ -123,13 +123,21 @@ func TestImportInvalidCredentialAws(t *testing.T) {
 }
 
 func TestSchemaCredentialAws(t *testing.T) {
-	for paramName, paramSchema := range resource.Schema {
-		required := paramName == "credentials"
-		computed := paramName != "credentials"
-		optional := paramName != "sid" && paramName != "credentials"
+	testCases := map[string]ExpectedParamSchema {
+		"credential": {true, false, false, false},
+		"sid": {false, false, true, false},
+		"account_sid": {false, false, true, true},
+		"friendly_name": {false, false, true, true},
+		"test_number": {false, false, true, true},
+		"url": {false, false, true, false},
+	}
 
-		assert.Equal(t, required, paramSchema.Required, fmt.Sprintf("schema.Required iff credentials: %s", paramName))
-		assert.Equal(t, computed, paramSchema.Computed, fmt.Sprintf("schema.Computed iff not credentials: %s", paramName))
-		assert.Equal(t, optional, paramSchema.Optional, fmt.Sprintf("schema.Optional iff not sid or credentials: %s", paramName))
+	for paramName, paramSchema := range resource.Schema {
+		if expectedSchema, ok := testCases[paramName]; ok {
+			assert.Equal(t, expectedSchema.Required, paramSchema.Required, fmt.Sprintf("schema.Required iff credentials: %s", paramName))
+			assert.Equal(t, expectedSchema.Computed, paramSchema.Computed, fmt.Sprintf("schema.Computed iff not credentials: %s", paramName))
+			assert.Equal(t, expectedSchema.Optional, paramSchema.Optional, fmt.Sprintf("schema.Optional iff param and not sid or credentials: %s", paramName))
+
+		}
 	}
 }
--- src/test/java/com/twilio/oai/TwilioTerraformGeneratorTest.java
+++ src/test/java/com/twilio/oai/TwilioTerraformGeneratorTest.java
@@ -9,14 +9,14 @@ import org.openapitools.codegen.config.CodegenConfigurator;
  * This test allows you to easily launch your code generation software under a debugger. Then run this test under debug
  * mode.  You will be able to step through your java code and then see the results in the out directory.
  */
-public class TwilioGoGeneratorTest {
+public class TwilioTerraformGeneratorTest {
 
     @Test
     public void launchCodeGenerator() {
         final CodegenConfigurator configurator = new CodegenConfigurator()
-            .setGeneratorName("twilio-go")
+            .setGeneratorName("terraform-provider-twilio")
             .setInputSpec("examples/twilio_api_v2010.yaml")
-            .setOutputDir("examples/go/rest");
+            .setOutputDir("examples/terraform/resources");
 
         final ClientOptInput clientOptInput = configurator.toClientOptInput();
         DefaultGenerator generator = new DefaultGenerator();
