bea241db46eb787586622f7fe41c09ff275bb939
Sam Harrison <sharrison@twilio.com>
2022-04-22 14:07:49 -0500

    [DI-2055] feat: support unmarshaling of numbers from strings in Go

--- examples/go-client/helper/rest/api/v2010/unit_test.go
+++ examples/go-client/helper/rest/api/v2010/unit_test.go
@@ -199,6 +199,35 @@ func TestObjectArrayTypeParam(t *testing.T) {
 	_, _ = twilio.CreateCredentialAws(&params)
 }
 
+func TestResponseDecodeTypes(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	testClient := NewMockBaseClient(mockCtrl)
+	twilio := NewApiServiceWithClient(testClient)
+
+	testClient.EXPECT().SendRequest(
+		gomock.Any(),
+		gomock.Any(),
+		gomock.Any(),
+		gomock.Any()).
+		DoAndReturn(func(method string, rawURL string, data url.Values, headers map[string]interface{}) (*http.Response, error) {
+			response := map[string]interface{}{
+				"test_number":       123.45,
+				"test_number_float": "123.45",
+			}
+
+			resp, _ := json.Marshal(response)
+
+			return &http.Response{Body: ioutil.NopCloser(bytes.NewReader(resp))}, nil
+		})
+
+	params := CreateCredentialAwsParams{}
+	resp, err := twilio.CreateCredentialAws(&params)
+
+	assert.Nil(t, err)
+	assert.Equal(t, float32(123.45), *resp.TestNumber)
+	assert.Equal(t, float32(123.45), *resp.TestNumberFloat)
+}
+
 func TestList(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	testClient := NewMockBaseClient(mockCtrl)
