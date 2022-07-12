dd85ea92a2bff78b2e4d2016ded6dbb322fac20a
sbansla <shobhitbansla@gmail.com>
2022-07-12 18:00:53 +0530

    created crudf operation methods partially

--- src/main/java/com/twilio/oai/TwilioCsharpGenerator.java
+++ src/main/java/com/twilio/oai/TwilioCsharpGenerator.java
@@ -1,19 +1,39 @@
 package com.twilio.oai;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.twilio.oai.common.ApplicationConstants;
+import com.twilio.oai.common.EnumConstants;
+import com.twilio.oai.common.Utility;
 import io.swagger.v3.oas.models.OpenAPI;
+import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.CodegenParameter;
+import org.openapitools.codegen.CodegenProperty;
 import org.openapitools.codegen.languages.CSharpClientCodegen;
+import org.openapitools.codegen.utils.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TwilioCsharpGenerator extends CSharpClientCodegen {
+
+    private DirectoryStructureService directoryStructureService = new DirectoryStructureService(EnumConstants.Generator.TWILIO_CSHARP);
+    String initialApiPackage;
+    private final List<CodegenModel> allModels = new ArrayList<>();
+    private  Map<String, String> modelFormatMap = new HashMap<>();
 
     public TwilioCsharpGenerator() {
         super();
 
         apiNameSuffix = "";
-
+        initialApiPackage = apiPackage;
         // Find the templates in the local resources dir.
         embeddedTemplateDir = templateDir = getName();
         sourceFolder = "";
@@ -23,20 +43,30 @@ public class TwilioCsharpGenerator extends CSharpClientCodegen {
     public void processOpts() {
         super.processOpts();
 
+        String[] inputSpecs = inputSpec.split("_");
+        final String version = inputSpecs[inputSpecs.length-1].replaceAll("\\.[^/]+$", "");
+        final String domain = String.join("", Arrays.copyOfRange(inputSpecs, 1, inputSpecs.length-1));
+
+        apiPackage = version.toUpperCase(); // Place the API files in the version folder.
+        additionalProperties.put("apiVersion", Utility.toFirstLetterCaps(version));
+        additionalProperties.put("apiVersionClass", version.toUpperCase());
+        additionalProperties.put("domain", StringUtils.camelize(domain));
+        additionalProperties.put("domainPackage", Utility.toFirstLetterCaps(domain));
+
         apiTestTemplateFiles.clear();
         apiDocTemplateFiles.clear();
         supportingFiles.clear();
         apiTemplateFiles.clear();
-        apiTemplateFiles.put("apiResource.mustache", "Resource.cs");
+        apiTemplateFiles.put("Resource.mustache", "Resource.cs");
+        apiTemplateFiles.put("Options.mustache", "Options.cs");
         modelTemplateFiles.clear();
         modelTestTemplateFiles.clear();
         modelDocTemplateFiles.clear();
-
     }
 
     @Override
     public void processOpenAPI(final OpenAPI openAPI) {
-
+        directoryStructureService.configure(openAPI, additionalProperties);
     }
 
     @Override
@@ -45,15 +75,161 @@ public class TwilioCsharpGenerator extends CSharpClientCodegen {
     }
 
     @Override
+    public String toParamName(String name) {
+        name = name.replace("<", "Before");
+        name = name.replace(">", "After");
+        name = super.toVarName(name);
+        return name;
+    }
+
+    @Override
+    public void postProcessModelProperty(CodegenModel model, CodegenProperty property) {
+        super.postProcessModelProperty(model, property);
+        property.isEnum =  property.isEnum && property.dataFormat == null;
+    }
+
+    public String toApiFilename(final String name) {
+        String[] split = super.toApiFilename(name).split(ApplicationConstants.PATH_SEPARATOR_PLACEHOLDER);
+        String apiFileName =  Arrays.stream(Arrays.copyOfRange(split, 0, split.length - 1))
+                .collect(Collectors.joining("/")) + "/"+split[split.length-1];
+        return apiFileName;
+    }
+
+    @Override
     public Map<String, Object> postProcessOperationsWithModels(final Map<String, Object> objs,
                                                                final List<Object> allModels) {
         final Map<String, Object> results = super.postProcessOperationsWithModels(objs, allModels);
+        final Map<String, Map<String, Object>> resources = new LinkedHashMap<>();
+
+        System.out.println("======= Operation =======");
+        final ArrayList<CodegenOperation> opList = getAllOperations(results);
+        for (final CodegenOperation co : opList) {
+            String path = co.path;
+
+            String[] filePathArray = co.baseName.split(ApplicationConstants.PATH_SEPARATOR_PLACEHOLDER);
+            String resourceName = filePathArray[filePathArray.length-1];
+            final Map<String, Object> resource = resources.computeIfAbsent(resourceName, k -> new LinkedHashMap<>());
+            populateCrudOperations(resource, co);
+
+            List<String> packagePaths = Arrays.asList(Arrays.copyOfRange(filePathArray,0 , filePathArray.length-1))
+                    .stream().collect(Collectors.toList());
+
+            // Add operations key to resource
+            final ArrayList<CodegenOperation> resourceOperationList = (ArrayList<CodegenOperation>) resource.computeIfAbsent("operations", k -> new ArrayList<>());
+            resourceOperationList.add(co);
+
+            co.allParams.stream().map(ConventionResolver::resolveParamTypes).map(item -> StringUtils.camelize(item.paramName)).collect(Collectors.toList());
+
+            System.out.println(co.operationId + "path: --> " + packagePaths);
+            if (packagePaths.isEmpty()) {
+                resource.put("packageSubPart", "");
+            } else {
+                String packagePath = packagePaths.stream().collect(Collectors.joining("."));
+                resource.put("packageSubPart", "."+packagePath);
+            }
+            if (co.operationId.equals("DeleteCall")) {
+                System.out.println("Path is: " +path);
+            }
+
+
+            results.put("recordKey", getRecordKey(opList, this.allModels));
+            resource.put("path", path);
+            resource.put("resourceName", resourceName);
+            resource.put("resourceConstant", "Resource");
+        }
+
+
+        results.put("resources", resources.values());
         return results;
     }
 
     @Override
+    public Map<String, Object> postProcessAllModels(final Map<String, Object> allModels) {
+        final Map<String, Object> results = super.postProcessAllModels(allModels);
+
+        for (final Object obj : results.values()) {
+            final Map<String, Object> mods = (Map<String, Object>) obj;
+            final ArrayList<Map<String, Object>> modList = (ArrayList<Map<String, Object>>) mods.get("models");
+
+            // Add all the models to the local models list.
+            modList
+                    .stream()
+                    .map(model -> model.get("model"))
+                    .map(CodegenModel.class::cast)
+                    .collect(Collectors.toCollection(() -> this.allModels));
+        }
+        setObjectFormatMap(this.allModels);
+        // Return an empty collection so no model files get generated.
+        return new HashMap<>();
+    }
+
+
+    private String getRecordKey(List<CodegenOperation> opList, List<CodegenModel> models) {
+        String recordKey =  "";
+        for (CodegenOperation co: opList) {
+            for(CodegenModel model: models) {
+                if(model.name.equals(co.returnType)) {
+                    recordKey = model.allVars
+                            .stream()
+                            .filter(v -> v.openApiType.equals("array"))
+                            .collect(Collectors.toList()).get(0).baseName;
+                }
+            }
+        }
+        return recordKey;
+    }
+
+    private ArrayList<CodegenOperation> getAllOperations(final Map<String, Object> results) {
+        final Map<String, Object> ops = getStringMap(results, "operations");
+         return (ArrayList<CodegenOperation>) ops.get("operation");
+    }
+
+    private void setObjectFormatMap(final List<CodegenModel> allModels) {
+        allModels.forEach(item -> {
+            ObjectMapper objectMapper = new ObjectMapper();
+            try {
+                JsonNode jsonNode = objectMapper.readTree(item.modelJson);
+                if (jsonNode.get("type").textValue().equals("object") && jsonNode.has("format")) {
+                    modelFormatMap.put(item.classFilename, jsonNode.get("format").textValue());
+                }
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void populateCrudOperations(final Map<String, Object> resource, final CodegenOperation operation) {
+        if (operation.nickname.startsWith(EnumConstants.Operation.CREATE.getValue())) {
+            resource.put("hasCreate", true);
+            operation.vendorExtensions.put("x-is-create-operation", true);
+            resource.put(EnumConstants.Operation.CREATE.name(), operation);
+        } else if (operation.nickname.startsWith(EnumConstants.Operation.FETCH.getValue())) {
+            resource.put("hasFetch", true);
+            resource.put(EnumConstants.Operation.FETCH.name(), operation);
+            operation.vendorExtensions.put("x-is-fetch-operation", true);
+        } else if (operation.nickname.startsWith(EnumConstants.Operation.UPDATE.getValue())) {
+            resource.put("hasUpdate", true);
+            operation.vendorExtensions.put("x-is-update-operation", true);
+            resource.put(EnumConstants.Operation.UPDATE.name(), operation);
+        } else if (operation.nickname.startsWith(EnumConstants.Operation.DELETE.getValue())) {
+            resource.put("hasDelete", true);
+            operation.vendorExtensions.put("x-is-delete-operation", true);
+            resource.put(EnumConstants.Operation.DELETE.name(), operation);
+        } else {
+            resource.put("hasRead", true);
+            operation.vendorExtensions.put("x-is-read-operation", true);
+            resource.put(EnumConstants.Operation.READ.name(), operation);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> getStringMap(final Map<String, Object> resource, final String key) {
+        return (Map<String, Object>) resource.computeIfAbsent(key, k -> new HashMap<>());
+    }
+
+    @Override
     public String getName() {
-        return "twilio-csharp";
+        return EnumConstants.Generator.TWILIO_CSHARP.getValue();
     }
 
     @Override
---
+++ src/main/java/com/twilio/oai/common/EnumConstants.java
@@ -0,0 +1,39 @@
+package com.twilio.oai.common;
+
+import lombok.AllArgsConstructor;
+
+public class EnumConstants {
+
+    public enum Generator {
+        TWILIO_JAVA("twilio-java"),
+        TWILIO_CSHARP("twilio-csharp");
+
+        private final String value;
+
+        public String getValue() {
+            return value;
+        }
+        private Generator(String value) {
+            this.value = value;
+        }
+    }
+
+    public enum Operation {
+        CREATE("Create"),
+        FETCH("Fetch"),
+        UPDATE("Update"),
+
+        DELETE("Delete"),
+
+        READ("Read");
+
+        private final String value;
+
+        public String getValue() {
+            return value;
+        }
+        private Operation(String value) {
+            this.value = value;
+        }
+    }
+}
