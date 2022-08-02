bb85bf7f973e1921cd0f3289236049eb4e4980e2
Bilal Boussayoud <bboussayoud@twilio.com>
2022-08-02 14:53:04 -0600

    test: add tests for node examples

--- examples/node/spec/unit/Accounts.spec.ts
+++ examples/node/spec/unit/Accounts.spec.ts
@@ -5,6 +5,14 @@ import Twilio from '../../lib/rest/Twilio';
 describe('accounts', () => {
     const twilio = new Twilio();
 
+    it('should create an account', () => {
+        const scope = nock('https://api.twilio.com')
+            .post('/v2010/2010-04-01/Accounts.json')
+            .reply(201, {account_sid: '123'});
+
+        return twilio.api.v2010.accounts.create({sid: '123'}).then(() => scope.done());
+    });
+
     it('should fetch an account', () => {
         const scope = nock('https://api.twilio.com')
             .get('/v2010/2010-04-01/Accounts/123.json')
@@ -12,4 +20,43 @@ describe('accounts', () => {
 
         return twilio.api.v2010.accounts('123').fetch().then(() => scope.done());
     });
+
+    it('should update an account', () => {
+        const scope = nock('https://api.twilio.com')
+            .post('/v2010/2010-04-01/Accounts/123.json')
+            .reply(200, {account_sid: '123', status: 'closed'});
+
+        return twilio.api.v2010.accounts('123').update({status: 'closed'}).then(() => scope.done());
+    });
+
+    it('should remove an account', () => {
+        const scope = nock('https://api.twilio.com')
+            .delete('/v2010/2010-04-01/Accounts/123.json')
+            .reply(204);
+
+        return twilio.api.v2010.accounts('123').remove().then(() => scope.done());
+    });
+});
+
+describe('calls', () => {
+    const twilio = new Twilio();
+
+    it('should fetch a call', () => {
+        const scope = nock('https://api.twilio.com')
+            .get('/v2010/2010-04-01/Accounts/123/Calls/1.json')
+            .reply(307, {account_sid: '123', sid: 1});
+
+        return twilio.api.v2010.accounts('123').calls('123', 1).fetch()
+            .then(() => scope.done());
+    });
+
+    it('should remove a call', () => {
+        const scope = nock('https://api.twilio.com')
+            .delete('/v2010/2010-04-01/Accounts/123/Calls/1.json')
+            .reply(204);
+
+        return twilio.api.v2010.accounts('123').calls('123', 1).remove()
+            .then(() => scope.done());
+    });
+
 });
