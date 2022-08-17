1e3959bce2e4f9ce62fa36bff252f850d81e89ee
Rohith Prakash <rprakash@twilio.com>
2022-08-17 21:20:04 +0530

    Made Comment.mustache

--- src/main/java/com/twilio/oai/TwilioCsharpGenerator.java
+++ src/main/java/com/twilio/oai/TwilioCsharpGenerator.java
@@ -366,25 +366,37 @@ public class TwilioCsharpGenerator extends CSharpClientCodegen {
     }
 
     private void populateCrudOperations(final Map<String, Object> resource, final CodegenOperation operation) {
+
         if (operation.nickname.startsWith(EnumConstants.Operation.CREATE.getValue())) {
             resource.put("hasCreate", true);
             operation.vendorExtensions.put("x-is-create-operation", true);
+            if(operation.notes != null)
+                operation.vendorExtensions.put("x-generate-comment",operation.notes);
             resource.put(EnumConstants.Operation.CREATE.name(), operation);
+
         } else if (operation.nickname.startsWith(EnumConstants.Operation.FETCH.getValue())) {
             resource.put("hasFetch", true);
             resource.put(EnumConstants.Operation.FETCH.name(), operation);
+            if(operation.notes != null)
+                operation.vendorExtensions.put("x-generate-comment",operation.notes);
             operation.vendorExtensions.put("x-is-fetch-operation", true);
         } else if (operation.nickname.startsWith(EnumConstants.Operation.UPDATE.getValue())) {
             resource.put("hasUpdate", true);
             operation.vendorExtensions.put("x-is-update-operation", true);
+            if(operation.notes != null)
+                operation.vendorExtensions.put("x-generate-comment",operation.notes);
             resource.put(EnumConstants.Operation.UPDATE.name(), operation);
         } else if (operation.nickname.startsWith(EnumConstants.Operation.DELETE.getValue())) {
             resource.put("hasDelete", true);
             operation.vendorExtensions.put("x-is-delete-operation", true);
+            if(operation.notes != null)
+                operation.vendorExtensions.put("x-generate-comment",operation.notes);
             resource.put(EnumConstants.Operation.DELETE.name(), operation);
         } else {
             resource.put("hasRead", true);
             operation.vendorExtensions.put("x-is-read-operation", true);
+            if(operation.notes != null)
+                operation.vendorExtensions.put("x-generate-comment",operation.notes);
             resource.put(EnumConstants.Operation.READ.name(), operation);
         }
     }
