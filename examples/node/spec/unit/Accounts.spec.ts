ac0004b027001b2f3546296ae42c2cab0cb65852
Matt Cole <mcole@twilio.com>
2022-08-30 11:51:22 -0400

    [DI-1403] Addressing PR Feedback

--- examples/node/spec/unit/Accounts.spec.ts
+++ examples/node/spec/unit/Accounts.spec.ts
@@ -40,12 +40,13 @@ describe('accounts', () => {
 
     it('should get account pages in between dates', () => {
         const scope = nock('https://api.twilio.com')
-            .get('/v2010/2010-04-01/Accounts.json')
+            .get('/v2010/2010-04-01/Accounts.json', 'DateCreated%3C=2022-12-25T00%3A00%3A00.000Z&DateCreated%3E=2022-01-01T00%3A00%3A00.000Z')
             .reply(200, {});
-        let params: AccountListInstancePageOptions = {dateCreatedBefore: new Date("2022-12-25"), dateCreatedAfter: new Date("2022-01-01")}
+        const params: AccountListInstancePageOptions = {dateCreatedBefore: new Date('2022-12-25'), dateCreatedAfter: new Date('2022-01-01')}
         return twilio.api.v2010.accounts.page(params).then(() => scope.done());
     })
 });
+
 
 describe('calls', () => {
     const twilio = new Twilio();
