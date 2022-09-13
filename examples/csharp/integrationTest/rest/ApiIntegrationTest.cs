78001a81b7939a6e945783f2fac6a97ab5904721
Isha Bansal <ibansal@twilio.com>
2022-09-13 16:50:59 +0530

    removed List arguments

--- examples/csharp/integrationTest/rest/ApiIntegrationTest.cs
+++ examples/csharp/integrationTest/rest/ApiIntegrationTest.cs
@@ -1,56 +1,36 @@
-using NSubstitute;
-using NSubstitute.ExceptionExtensions;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Twilio;
-using Twilio.Clients;
-using Twilio.Converters;
-using Twilio.Exceptions;
-using Twilio.Http;
 using Twilio.Rest.Api.V2010;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Rest.Api.V2010.Account.Call;
 using Twilio.Rest.Api.V2010.Credential;
 
-namespace examples.csharp.integrationTest.rest
+namespace Twilio.Tests.Rest
 {
     [TestFixture]
     public class ApiIntegrationTest
     {
-        readonly String baseURL = "http://prism_twilio:4010";
-        readonly String ACCOUNT_SID = "AC12345678123456781234567812345678";
+        readonly string baseURL = "http://prism_twilio:4010";
+        readonly string ACCOUNT_SID = "AC12345678123456781234567812345678";
 
         [SetUp]
         public void SetUp()
         {
-            String authToken = "CR12345678123456781234567812345678";
+            string authToken = "CR12345678123456781234567812345678";
             TestRestClient testClient = new TestRestClient(ACCOUNT_SID, authToken,baseURL);
             TwilioClient.Init(ACCOUNT_SID, authToken);
             TwilioClient.SetRestClient(testClient);
         }
-        
-        // [Test]
-        // public void TestGet() {
-        //     var resource = AccountResource.GetPage();
-        //     Assert.NotNull(resource);
-        //     Assert.AreEqual("Ahoy", resource.Records[0].TestString());
-        // }
 
-//        [Test]
-//        public void TestEnum()
-//        {
-//            var recordingStatusCallbackEvent = new List<string>()
-//            {
-//                "http://test1.com/"
-//            };
-//            string[] arr={ "http://test1.com/" };
-//            var response = AccountResource.Create("true",new Uri("https://example.com"),arr.ToList());
-//            Assert.NotNull(response);
-//            Assert.AreEqual(response.TestEnum, "completed");
-//        }
+        [Test]
+        public void TestEnum()
+        {
+            var response = AccountResource.Create("true",new Uri("https://example.com"));
+            Assert.NotNull(response);
+            Assert.AreEqual(response.TestEnum.ToString(), "completed");
+        }
         
         [Test]
         public void TestDelete() {
@@ -77,40 +57,30 @@ namespace examples.csharp.integrationTest.rest
             Assert.True(call.TestObject.Voice);
         }
         
-//        [Test]
-//        public void TestPost()
-//        {
-//            var call = CallResource.Create("testString", ACCOUNT_SID, new List<string>() { "Hello", "from", "Twilio !" });
-//            Assert.NotNull(call);
-//            Assert.AreEqual("Ahoy", call.TestString);
-//        }
+        [Test]
+        public void TestPost()
+        {
+            var call = CallResource.Create("testString", ACCOUNT_SID);
+            Assert.NotNull(call);
+            Assert.AreEqual("Ahoy", call.TestString);
+        }
         
-//        [Test]
-//        public void TestDateTimeQueryParam()
-//        {
-//            var response = AccountResource.Read(dateTest:DateTime.Now,dateCreatedBefore: DateTime.Now.AddMonths(-2),dateCreatedAfter: DateTime.Now.AddMonths(2),pageSize:5);
-//            Assert.NotNull(response);
-//            var enumerator = response.GetEnumerator();
-//            Assert.NotNull(enumerator);
-//            enumerator.MoveNext();
-//            var firstResult = enumerator.Current;
-//            Assert.NotNull(firstResult);
-//            Assert.AreEqual("Ahoy", firstResult.TestString);
-//            enumerator.MoveNext();
-//            var secondResult = enumerator.Current;
-//            Assert.NotNull(secondResult);
-//            Assert.AreEqual("Matey", secondResult.TestString);
-//
-//            List<String> testStringValues = new List<String>();
-//
-//            foreach (AccountResource res in response)
-//            {
-//                Assert.NotNull(res);
-//                testStringValues.Add(res.TestString);
-//            }
-//            Assert.AreEqual(testStringValues[0], "Ahoy");
-//            Assert.AreEqual(testStringValues[1], "Matey");
-//        }
+        [Test]
+        public void TestDateTimeQueryParam()
+        {
+            var response = AccountResource.Read(dateTest:DateTime.Now,dateCreatedBefore: DateTime.Now.AddMonths(-2),dateCreatedAfter: DateTime.Now.AddMonths(2),pageSize:5);
+            Assert.NotNull(response);
+            var enumerator = response.GetEnumerator();
+            Assert.NotNull(enumerator);
+            enumerator.MoveNext();
+            var firstResult = enumerator.Current;
+            Assert.NotNull(firstResult);
+            Assert.AreEqual("Ahoy", firstResult.TestString);
+            enumerator.MoveNext();
+            var secondResult = enumerator.Current;
+            Assert.NotNull(secondResult);
+            Assert.AreEqual("Matey", secondResult.TestString);
+        }
         
         [Test]
         public void TestUpdate() {
@@ -130,21 +100,19 @@ namespace examples.csharp.integrationTest.rest
             Assert.AreEqual("issue description", response.TestArrayOfObjects[0].Description);
         }
 
-//        [Test]
-//        public void TestCreateNewcredentials()
-//        {
-//            NewCredentialsResource.PermissionsEnum demoEnum= "get-all";
-//            var permissions = new List<NewCredentialsResource.PermissionsEnum>() { demoEnum };
-//            NewCredentialsResource response = NewCredentialsResource.Create("test",testEnum:NewCredentialsResource.StatusEnum.Paused,permissions:permissions);
-//            Assert.NotNull(response);
-//        }
-//
+        [Test]
+        public void TestCreateNewcredentials()
+        {
+            NewCredentialsResource response = NewCredentialsResource.Create("test",testEnum:NewCredentialsResource.StatusEnum.Paused);
+            Assert.NotNull(response);
+        }
+
         [Test]
         public void TestPageLimitEqualToZero()
         {
             var response = AwsResource.Read(1,0);
             Assert.NotNull(response);
-            List<String> testStringValues = new List<String>();
+            List<string> testStringValues = new List<string>();
             foreach (AwsResource res in response)
             {
                 Assert.NotNull(res);
