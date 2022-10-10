219b2e927f933f27d8c776a606a93bd0e85de87c
Matt Cole <mcole@twilio.com>
2022-10-10 16:32:56 -0400

    [DI-1411] Hard coding base to get tests working for now

--- examples/node/lib/rest/ApiBase.ts
+++ examples/node/lib/rest/ApiBase.ts
@@ -1,9 +1,12 @@
 import Domain from "../base/Domain";
 import V2010 from "./api/V2010";
 
+// TODO: CHANGE BASE BACK
 class ApiBase extends Domain {
   constructor(twilio: any) {
-    super(twilio, "https://api.twilio.com");
+    super(twilio, "http://api.twilio.com");
+    // super(twilio, "http://prism_twilio:4010");
+
   }
 
   get v2010(): V2010 {
