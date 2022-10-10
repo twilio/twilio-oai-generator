a42d20c1f67cd6ddf8243396515f3c5c92a7f0b5
Matt Cole <mcole@twilio.com>
2022-10-10 15:36:02 -0400

    [DI-1411] Base integration tests for v2010 spec

--- examples/node/spec/integration/apiIntegration.spec.ts
+++ examples/node/spec/integration/apiIntegration.spec.ts
@@ -1,39 +1,42 @@
 'use strict'
 import Twilio from '../../lib/rest/Twilio';
-import {AccountListInstancePageOptions} from "../../lib/rest/api/v2010/account";
+import {AccountListInstanceCreateOptions, AccountListInstancePageOptions} from "../../lib/rest/api/v2010/account";
 
-describe('', () => {
-    beforeEach( () => {
-        console.log("Before Each called :)");
-        });
-    it('should page an account', (done) => {
+// // TODO: Need to change baseUrl from https://api.twilio.com to http://api.twilio.com (change scheme to http)
+// //  Options: jasmine spy or create custom Twilio test object
+describe('Integration tests', () => {
+    const accountSid:string = 'AC12345678123456781234567812345678';
+    const authToken:string = 'CR12345678123456781234567812345678';
+    const client = new Twilio(accountSid, authToken);
 
-        // TODO: Move all of this to beforeEach
-        const accountSid:string = 'AC12345678123456781234567812345678';
-        const authToken:string = 'CR12345678123456781234567812345678';
-        // TODO: Need to change baseUrl from https://api.twilio.com to http://prism_twilio:4010
-        // Options: jasmine spy or create custom Twilio test object
-        const client:Twilio = new Twilio(accountSid, authToken);
-        // TODO: Determine why spyOn method isn't available
-        // const propertySpy = spyOn(client.api, "baseUrl").and.returnValue('http://prism_twilio:4010');
-
-        const params: AccountListInstancePageOptions =  {
-            dateCreated: new Date(),
-            dateTest: '',
-            dateCreatedBefore: new Date(),
-            dateCreatedAfter: new Date(),
-            pageSize: 1,
-        };
-
-        const promise = client.api.v2010.accounts.page(params);
-        promise.then((response) => {
-            console.log(response);
-            expect(response).toBeDefined();
-            expect(response.accountSid).toEqual(accountSid);
-            expect(response.testString).toEqual('Ahoy');
-            done();
-        }, () => {
-            throw new Error('failed');
-        }).done();
+    xit('should get an account', async () => {
+        const params = {};
+        const result = await client.api.v2010.accounts.page();
     })
+
+    xit('should create an account', async () => {
+        const result = await client.api.v2010.accounts.create();
+        expect(result.sid).toEqual("CR12345678123456781234567812345678");
+        expect(result.testString).toEqual("Ahoy");
+    })
+
+    xit('should create a call ', async () => {
+        const params = {};
+        const result = await client.api.v2010.accounts(accountSid).calls.create(params);
+        expect(result.sid).toEqual("CR12345678123456781234567812345678");
+        expect(result.testString).toEqual("Ahoy");
+    })
+
+    it('should fetch a call', async () => {
+        const result = await client.api.v2010.accounts(accountSid).calls(123).fetch();
+        expect(result.sid).toEqual("CR12345678123456781234567812345678");
+        expect(result.testString).toEqual("Ahoy");
+    })
+
+    xit('should update call feedback summary', async () => {
+        const params = {};
+        const result = await client.api.v2010.accounts(accountSid).calls.feedback_call_summary("CR12345678123456781234567812345678").update(params);
+        expect(result.testArrayOfObjects[0].description).toEqual("issue description")
+    })
+
 })
