60fb3cd9b24fbed7765c68fdddb0560a6551b3d4
Athira Sabu <102021496+AsabuHere@users.noreply.github.com>
2022-10-14 11:09:40 +0530

    fix: Remove pagination methods from resources where its not necessary (#274)
    * Remove pagination methods from resources where its not necessary

--- examples/csharp/src/Twilio/Rest/Api/V2010/AccountResource.cs
+++ examples/csharp/src/Twilio/Rest/Api/V2010/AccountResource.cs
@@ -366,6 +366,56 @@ namespace Twilio.Rest.Api.V2010
         }
         #endif
 
+        
+        /// <summary> Fetch the target page of records </summary>
+        /// <param name="targetUrl"> API-generated URL for the requested results page </param>
+        /// <param name="client"> Client to make requests to Twilio </param>
+        /// <returns> The target page of records </returns>
+        public static Page<AccountResource> GetPage(string targetUrl, ITwilioRestClient client)
+        {
+            client = client ?? TwilioClient.GetRestClient();
+
+            var request = new Request(
+                HttpMethod.Get,
+                targetUrl
+            );
+
+            var response = client.Request(request);
+            return Page<AccountResource>.FromJson("accounts", response.Content);
+        }
+
+        /// <summary> Fetch the next page of records </summary>
+        /// <param name="page"> current page of records </param>
+        /// <param name="client"> Client to make requests to Twilio </param>
+        /// <returns> The next page of records </returns>
+        public static Page<AccountResource> NextPage(Page<AccountResource> page, ITwilioRestClient client)
+        {
+            var request = new Request(
+                HttpMethod.Get,
+                page.GetNextPageUrl(Rest.Domain.Api)
+            );
+
+            var response = client.Request(request);
+            return Page<AccountResource>.FromJson("accounts", response.Content);
+        }
+
+        /// <summary> Fetch the previous page of records </summary>
+        /// <param name="page"> current page of records </param>
+        /// <param name="client"> Client to make requests to Twilio </param>
+        /// <returns> The previous page of records </returns>
+        public static Page<AccountResource> PreviousPage(Page<AccountResource> page, ITwilioRestClient client)
+        {
+            var request = new Request(
+                HttpMethod.Get,
+                page.GetPreviousPageUrl(Rest.Domain.Api)
+            );
+
+            var response = client.Request(request);
+            return Page<AccountResource>.FromJson("accounts", response.Content);
+        }
+
+
+
 
 
 
@@ -449,53 +499,6 @@ namespace Twilio.Rest.Api.V2010
         }
         #endif
     
-        /// <summary> Fetch the target page of records </summary>
-        /// <param name="targetUrl"> API-generated URL for the requested results page </param>
-        /// <param name="client"> Client to make requests to Twilio </param>
-        /// <returns> The target page of records </returns>
-        public static Page<AccountResource> GetPage(string targetUrl, ITwilioRestClient client)
-        {
-            client = client ?? TwilioClient.GetRestClient();
-
-            var request = new Request(
-                HttpMethod.Get,
-                targetUrl
-            );
-
-            var response = client.Request(request);
-            return Page<AccountResource>.FromJson("accounts", response.Content);
-        }
-
-        /// <summary> Fetch the next page of records </summary>
-        /// <param name="page"> current page of records </param>
-        /// <param name="client"> Client to make requests to Twilio </param>
-        /// <returns> The next page of records </returns>
-        public static Page<AccountResource> NextPage(Page<AccountResource> page, ITwilioRestClient client)
-        {
-            var request = new Request(
-                HttpMethod.Get,
-                page.GetNextPageUrl(Rest.Domain.Api)
-            );
-
-            var response = client.Request(request);
-            return Page<AccountResource>.FromJson("accounts", response.Content);
-        }
-
-        /// <summary> Fetch the previous page of records </summary>
-        /// <param name="page"> current page of records </param>
-        /// <param name="client"> Client to make requests to Twilio </param>
-        /// <returns> The previous page of records </returns>
-        public static Page<AccountResource> PreviousPage(Page<AccountResource> page, ITwilioRestClient client)
-        {
-            var request = new Request(
-                HttpMethod.Get,
-                page.GetPreviousPageUrl(Rest.Domain.Api)
-            );
-
-            var response = client.Request(request);
-            return Page<AccountResource>.FromJson("accounts", response.Content);
-        }
-
         /// <summary>
         /// Converts a JSON string into a AccountResource object
         /// </summary>
@@ -512,7 +515,6 @@ namespace Twilio.Rest.Api.V2010
                 throw new ApiException(e.Message, e);
             }
         }
-
 
     
         ///<summary> The account_sid </summary> 
--- examples/csharp/src/Twilio/Rest/FlexApi/V1/CallResource.cs
+++ examples/csharp/src/Twilio/Rest/FlexApi/V1/CallResource.cs
@@ -104,53 +104,6 @@ namespace Twilio.Rest.FlexApi.V1
         }
         #endif
     
-        /// <summary> Fetch the target page of records </summary>
-        /// <param name="targetUrl"> API-generated URL for the requested results page </param>
-        /// <param name="client"> Client to make requests to Twilio </param>
-        /// <returns> The target page of records </returns>
-        public static Page<CallResource> GetPage(string targetUrl, ITwilioRestClient client)
-        {
-            client = client ?? TwilioClient.GetRestClient();
-
-            var request = new Request(
-                HttpMethod.Get,
-                targetUrl
-            );
-
-            var response = client.Request(request);
-            return Page<CallResource>.FromJson("", response.Content);
-        }
-
-        /// <summary> Fetch the next page of records </summary>
-        /// <param name="page"> current page of records </param>
-        /// <param name="client"> Client to make requests to Twilio </param>
-        /// <returns> The next page of records </returns>
-        public static Page<CallResource> NextPage(Page<CallResource> page, ITwilioRestClient client)
-        {
-            var request = new Request(
-                HttpMethod.Get,
-                page.GetNextPageUrl(Rest.Domain.Api)
-            );
-
-            var response = client.Request(request);
-            return Page<CallResource>.FromJson("", response.Content);
-        }
-
-        /// <summary> Fetch the previous page of records </summary>
-        /// <param name="page"> current page of records </param>
-        /// <param name="client"> Client to make requests to Twilio </param>
-        /// <returns> The previous page of records </returns>
-        public static Page<CallResource> PreviousPage(Page<CallResource> page, ITwilioRestClient client)
-        {
-            var request = new Request(
-                HttpMethod.Get,
-                page.GetPreviousPageUrl(Rest.Domain.Api)
-            );
-
-            var response = client.Request(request);
-            return Page<CallResource>.FromJson("", response.Content);
-        }
-
         /// <summary>
         /// Converts a JSON string into a CallResource object
         /// </summary>
@@ -167,7 +120,6 @@ namespace Twilio.Rest.FlexApi.V1
                 throw new ApiException(e.Message, e);
             }
         }
-
 
     
         ///<summary> The sid </summary> 
