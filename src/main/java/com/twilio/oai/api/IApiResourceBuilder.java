e1de886d8962c7e0b2fc8d3333b708a11ad90246
Isha Bansal <ishabansal2467@gmail.com>
2022-11-23 12:30:37 +0530

    chore: resolve PR feedback

--- src/main/java/com/twilio/oai/api/IApiResourceBuilder.java
+++ src/main/java/com/twilio/oai/api/IApiResourceBuilder.java
@@ -6,6 +6,7 @@ import org.openapitools.codegen.CodegenParameter;
 import org.openapitools.codegen.CodegenProperty;
 
 public interface IApiResourceBuilder {
+    IApiResourceBuilder updateTemplate();
     IApiResourceBuilder updateOperations(ISchemaResolver<CodegenParameter> codegenParameterIResolver);
     IApiResourceBuilder updateResponseModel(ISchemaResolver<CodegenProperty> codegenPropertyIResolver);
     IApiResourceBuilder updateAdditionalProps(DirectoryStructureService directoryStructureService);
