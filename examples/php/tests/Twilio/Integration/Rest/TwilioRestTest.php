9a304057caa315a348588c9fe6c19218fdc65f9a
Rohith Prakash <rohithprakashklm@gmail.com>
2022-12-06 16:54:29 +0530

    fix: Fix Php Page Template and add tests for Read method (#356)
    <!--
    We appreciate the effort for this pull request but before that please
    make sure you read the contribution guidelines, then fill out the blanks
    below.
    
    Please format the PR title appropriately based on the type of change:
      <type>[!]: <description>
    Where <type> is one of: docs, chore, feat, fix, test, misc.
    Add a '!' after the type for breaking changes (e.g. feat!: new breaking
    feature).
    
    **All third-party contributors acknowledge that any contributions they
    provide will be made under the same open-source license that the
    open-source project is provided under.**
    
    Please enter each Issue number you are resolving in your PR after one of
    the following words [Fixes, Closes, Resolves]. This will auto-link these
    issues and close them when this PR is merged!
    e.g.
    Fixes #1
    Closes #2
    -->
    
    # Fixes #
    
    Fixes Page Class Template for php and add tests for read method
    
    ### Checklist
    - [x] I acknowledge that all my contributions will be made under the
    project's license
    - [ ] Run `make test-docker`
    - [ ] Verify affected language:
    - [ ] Generate [twilio-go](https://github.com/twilio/twilio-go) from our
    [OpenAPI specification](https://github.com/twilio/twilio-oai) using the
    [build_twilio_go.py](./examples/build_twilio_go.py) using `python
    examples/build_twilio_go.py path/to/twilio-oai/spec/yaml
    path/to/twilio-go` and inspect the diff
        - [ ] Run `make test` in `twilio-go`
        - [ ] Create a pull request in `twilio-go`
        - [ ] Provide a link below to the pull request
    - [ ] I have made a material change to the repo (functionality, testing,
    spelling, grammar)
    - [ ] I have read the [Contribution
    Guidelines](https://github.com/twilio/twilio-oai-generator/blob/main/CONTRIBUTING.md)
    and my PR follows them
    - [ ] I have titled the PR appropriately
    - [ ] I have updated my branch with the main branch
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have added the necessary documentation about the functionality
    in the appropriate .md file
    - [ ] I have added inline documentation to the code I modified
    
    If you have questions, please create a GitHub Issue in this repository.

--- examples/php/tests/Twilio/Integration/Rest/TwilioRestTest.php
+++ examples/php/tests/Twilio/Integration/Rest/TwilioRestTest.php
@@ -121,7 +121,7 @@ class TwilioRestTest extends HolodeckTestCase {
 
         $response = $this->twilio->api->v2010->accounts("AC222222222222222222222222222222")->calls
             ->feedbackCallSummary("FSXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")->
-                update(new \DateTime('2022-12-04'), new \DateTime('2022-12-01'));
+            update(new \DateTime('2022-12-04'), new \DateTime('2022-12-01'));
 
         $this->assertNotNull($response);
         $this->assertRequest(new Request(
@@ -132,4 +132,144 @@ class TwilioRestTest extends HolodeckTestCase {
         ));
     }
 
+    public function testReadRequest(): void
+    {
+        $this->holodeck->mock(new Response(500, ''));
+
+        try {
+            $this->twilio->api->v2010->accounts->read();
+        } catch (DeserializeException $e) {
+        } catch (TwilioException $e) {
+        }
+
+        $this->assertRequest(new Request(
+            'get',
+            'https://api.twilio.com/2010-04-01/Accounts.json'
+        ));
+    }
+
+    public function testReadEmptyResponse(): void
+    {
+        $this->holodeck->mock(new Response(
+            200,
+            '
+            {
+                "first_page_uri": "/2010-04-01/Accounts.json?Page=0",
+                "end": 0,
+                "previous_page_uri": null,
+                "accounts": [],
+                "uri": "/2010-04-01/Accounts.json?Page=0",
+                "page_size": 50,
+                "start": 0,
+                "next_page_uri": null,
+                "page": 0
+            }
+            '
+        ));
+
+        $actual = $this->twilio->api->v2010->accounts->read();
+
+        $this->assertNotNull($actual);
+    }
+
+    public function testReadFullResponse(): void
+    {
+        $this->holodeck->mock(new Response(
+            200,
+            '
+            {
+                "first_page_uri": "/2010-04-01/Accounts.json?FriendlyName=friendly_name&Status=active&PageSize=50&Page=0",
+                "end": 0,
+                "previous_page_uri": null,
+                "accounts": [
+                   {
+                        "account_sid": "account_sid",
+                        "test_date_time": "Thu, 30 Jul 2015 20:00:00 +0000",
+                        "test_string": "Test  String",
+                        "sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                    } 
+                ],
+                "uri": "/2010-04-01/Accounts.json?DateCreated=date_created",
+                "page_size": 50,
+                "start": 0,
+                "next_page_uri": null,
+                "page": 0
+            }
+            '
+        ));
+        $actual = $this->twilio->api->v2010->accounts->read();
+        $this->assertGreaterThan(0, \count($actual));
+    }
+
+    public function testReadStringMessage(): void
+    {
+        $this->holodeck->mock(new Response(
+            200,
+            '
+            {
+                "first_page_uri": "/2010-04-01/Accounts.json?FriendlyName=friendly_name&Status=active&PageSize=50&Page=0",
+                "end": 0,
+                "previous_page_uri": null,
+                "accounts": [
+                   {
+                        "account_sid": "account_sid",
+                        "test_date_time": "Thu, 30 Jul 2015 20:00:00 +0000",
+                        "test_string": "Test  String",
+                        "sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                    } 
+                ],
+                "uri": "/2010-04-01/Accounts.json?DateCreated=date_created",
+                "page_size": 50,
+                "start": 0,
+                "next_page_uri": null,
+                "page": 0
+            }
+            '
+        ));
+        $actual = $this->twilio->api->v2010->accounts->read();
+        $val = array_values($actual)[0];
+        $this->assertEquals(
+            $val->testString,
+            "Test  String",
+        );
+    }
+
+    public function testShouldPrefixedMapReadDateRange(): void
+    {
+
+        $this->holodeck->mock(new Response(200,
+            '
+            {
+                "first_page_uri": "/2010-04-01/Accounts.json",
+                "end": 0,
+                "previous_page_uri": null,
+                "accounts": [
+                   {
+                        "account_sid": "account_sid",
+                        "test_date_time": "Thu, 30 Jul 2015 20:00:00 +0000",
+                        "test_string": "Test  String",
+                        "sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                    } 
+                ],
+                "uri": "/2010-04-01/Accounts.json?DateCreated=date_created",
+                "page_size": 50,
+                "start": 0,
+                "next_page_uri": null,
+                "page": 0
+            }
+            '
+        ));
+
+        $actual = $this->twilio->api->v2010->accounts->read(
+            [
+                'dateCreatedBefore' => "2011-05-21",
+                'dateCreatedAfter' => "2012-01-01"
+            ]);
+        $this->assertNotNull($actual);
+        $this->assertRequest(new Request(
+            'get',
+            "https://api.twilio.com/2010-04-01/Accounts.json",
+            ['DateCreated<' => '2011-05-21', 'DateCreated>' => "2012-01-01"]
+        ));
+    }
 }
