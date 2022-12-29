a15f43bbcdf0a5787e94f251032cb418f7f2344c
cchua <cchua@twilio.com>
2022-12-29 12:09:49 -0700

    [DI-2426] generate examples

--- examples/node/lib/rest/api/v2010/account.ts
+++ examples/node/lib/rest/api/v2010/account.ts
@@ -644,7 +644,7 @@ export function AccountListInstance(version: V2010): AccountListInstance {
       );
     if (params["pageSize"] !== undefined) data["PageSize"] = params["pageSize"];
 
-    if (params.page !== undefined) data["Page"] = params.pageNumber;
+    if (params.pageNumber !== undefined) data["Page"] = params.pageNumber;
     if (params.pageToken !== undefined) data["PageToken"] = params.pageToken;
 
     const headers: any = {};
