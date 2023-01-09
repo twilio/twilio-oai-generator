6c7f6347d9279b9596d2d1ea61f8aa1d6041f7b9
sburman <sburman@twilio.com>
2023-01-09 23:12:45 +0530

    implementing resolvers in ruby

--- src/main/java/com/twilio/oai/TwilioRubyGenerator.java
+++ src/main/java/com/twilio/oai/TwilioRubyGenerator.java
@@ -1,13 +1,29 @@
 package com.twilio.oai;
 
+import com.twilio.oai.api.ApiResources;
+import com.twilio.oai.api.RubyApiResourceBuilder;
+import com.twilio.oai.common.EnumConstants;
+import com.twilio.oai.common.Utility;
 import com.twilio.oai.resolver.IConventionMapper;
 import com.twilio.oai.resolver.LanguageConventionResolver;
+import com.twilio.oai.resolver.LanguageParamResolver;
+import com.twilio.oai.resolver.LanguagePropertyResolver;
+import com.twilio.oai.resolver.common.CodegenModelResolver;
 import com.twilio.oai.resolver.ruby.RubyCaseResolver;
-import com.twilio.oai.resolver.ruby.RubyParameterResolver;
+import com.twilio.oai.resource.IResourceTree;
+import com.twilio.oai.resource.ResourceMap;
+import com.twilio.oai.template.RubyApiActionTemplate;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.CodegenOperation;
 import org.openapitools.codegen.languages.RubyClientCodegen;
+import org.openapitools.codegen.model.ModelMap;
 import org.openapitools.codegen.model.ModelsMap;
+import org.openapitools.codegen.model.OperationsMap;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.twilio.oai.common.ApplicationConstants.CONFIG_RUBY_JSON_PATH;
@@ -41,8 +57,7 @@ public class TwilioRubyGenerator extends RubyClientCodegen {
 
     @Override
     public String apiFilename(final String templateName, final String tag) {
-        String filename = rubyApiActionTemplate.apiFilename(templateName, super.apiFilename(templateName, tag));
-        return filename;
+        return rubyApiActionTemplate.apiFilename(templateName, super.apiFilename(templateName, tag));
     }
 
     @Override
@@ -79,10 +94,14 @@ public class TwilioRubyGenerator extends RubyClientCodegen {
     private ApiResources generateResources(final List<CodegenOperation> opList) {
         updateApiVersion(directoryStructureService);
         final IConventionMapper conventionMapper = new LanguageConventionResolver(CONFIG_RUBY_JSON_PATH);
-        new RubyParameterResolver(conventionMapper);
+        final CodegenModelResolver codegenModelResolver = new CodegenModelResolver(conventionMapper,
+                modelFormatMap,
+                List.of(EnumConstants.RubyDataTypes.values()));
         return new RubyApiResourceBuilder(rubyApiActionTemplate, opList, allModels, directoryStructureService)
                 .updateApiPath()
+                .updateOperations(new LanguageParamResolver(conventionMapper))
                 .updateTemplate()
+                .updateResponseModel(new LanguagePropertyResolver(conventionMapper), codegenModelResolver)
                 .build();
     }
 
--- src/main/java/com/twilio/oai/resolver/ruby/RubyCaseResolver.java
+++ src/main/java/com/twilio/oai/resolver/ruby/RubyCaseResolver.java
@@ -1,17 +1,17 @@
 package com.twilio.oai.resolver.ruby;
 
-import com.twilio.oai.StringHelper;
 import com.twilio.oai.resolver.CaseResolver;
+import org.openapitools.codegen.utils.StringUtils;
 
 public class RubyCaseResolver implements CaseResolver {
     @Override
     public String productOperation(final String product) {
-        return StringHelper.toSnakeCase(product);
+        return StringUtils.underscore(product);
     }
 
     @Override
     public String pathOperation(final String pathPart) {
-        return StringHelper.toSnakeCase(pathPart);
+        return StringUtils.underscore(pathPart);
     }
 
     @Override
