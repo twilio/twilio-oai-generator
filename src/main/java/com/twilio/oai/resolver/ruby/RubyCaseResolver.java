d15eabd888ac9dc2c3a450eaacc88b862c5ffda7
sburman <sburman@twilio.com>
2023-01-11 10:30:35 +0530

    Update RubyCaseResolver.java

--- src/main/java/com/twilio/oai/resolver/ruby/RubyCaseResolver.java
+++ src/main/java/com/twilio/oai/resolver/ruby/RubyCaseResolver.java
@@ -1,17 +1,17 @@
 package com.twilio.oai.resolver.ruby;
 
+import com.twilio.oai.StringHelper;
 import com.twilio.oai.resolver.CaseResolver;
-import org.openapitools.codegen.utils.StringUtils;
 
 public class RubyCaseResolver implements CaseResolver {
     @Override
     public String productOperation(final String product) {
-        return StringUtils.underscore(product);
+        return StringHelper.toSnakeCase(product);
     }
 
     @Override
     public String pathOperation(final String pathPart) {
-        return StringUtils.underscore(pathPart);
+        return StringHelper.toSnakeCase(pathPart);
     }
 
     @Override
