f8620a0a926f27a391f978f6f5cd08d6a0ed5f7b
Matt Cole <mcole@twilio.com>
2023-01-13 10:50:01 -0500

    [DI-2453] test: fix issue where tests could not find twilio module

--- examples/python/tests/unit/account_test.py
+++ examples/python/tests/unit/account_test.py
@@ -1,3 +1,9 @@
+from twilio.rest import Client
+
+accountSid = "AC12345678123456781234567812345678"
+authToken = "CR12345678123456781234567812345678"
+twilio = Client(accountSid, authToken)
+
 def func(x):
     return x + 1
 
