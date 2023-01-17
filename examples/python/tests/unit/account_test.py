f841bfe2dae5d2ae516c8b2617582a22cc34cbef
Matt Cole <mcole@twilio.com>
2023-01-17 11:44:01 -0500

    chore: remove testing changes

--- examples/python/tests/unit/account_test.py
+++ examples/python/tests/unit/account_test.py
@@ -1,20 +1,6 @@
-from twilio.rest import Client
-
-accountSid = "AC12345678123456781234567812345678"
-authToken = "CR12345678123456781234567812345678"
-twilio = Client(accountSid, authToken)
-
-
-def create_account():
-    response = twilio.api.v2010.accounts.create()
-    print(f'Response: {response}')
-
-
 def func(x):
     return x + 1
 
 # Placeholder test
 def test_answer():
     assert func(3) == 4
-
-create_account()
