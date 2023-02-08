d46d25d0f3a1ffcde0da55ea44535d1cbb7ae4bb
sbansla <sbansla@twilio.com>
2023-02-08 16:34:47 +0530

    removed parameter map from enum

--- src/main/java/com/twilio/oai/resolver/csharp/ParameterFormat.java
+++ src/main/java/com/twilio/oai/resolver/csharp/ParameterFormat.java
@@ -1,5 +1,6 @@
 package com.twilio.oai.resolver.csharp;
 
+import com.twilio.oai.common.ApplicationConstants;
 import com.twilio.oai.common.EnumConstants;
 import com.twilio.oai.resolver.DataSanitizer;
 import org.apache.commons.lang3.StringUtils;
@@ -13,8 +14,7 @@ public class ParameterFormat implements DataSanitizer<CodegenParameter> {
     public void sanitize(CodegenParameter parameter) {
         if (parameter.dataFormat == null) return;
         if (parameter.isMap && StringUtils.isNotBlank(parameter.dataFormat)
-                && Arrays.stream(EnumConstants.ParameterMaps.values())
-                .anyMatch(value -> parameter.dataFormat.startsWith(value.getValue()))) {
+                && parameter.dataFormat.startsWith(ApplicationConstants.PREFIXED_COLLAPSIBLE_MAP)) {
             // Example: dataformat = prefixed-collapsible-map-AddOns
             List<String> splitDataFormat = new ArrayList<>(List.of(parameter.dataFormat.split("-")));
             parameter.vendorExtensions.put("x-map-value", splitDataFormat.get(splitDataFormat.size()-1));
