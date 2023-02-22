e328ec9f87a35d9a1355503c0cf4c56f2466a347
Claudia Chua <43133161+claudiachua@users.noreply.github.com>
2023-02-22 15:53:05 -0700

    fix: import statements (#445)

--- examples/python/twilio/rest/api/v2010/account/__init__.py
+++ examples/python/twilio/rest/api/v2010/account/__init__.py
@@ -21,7 +21,7 @@ from twilio.base.instance_resource import InstanceResource
 from twilio.base.list_resource import ListResource
 from twilio.base.version import Version
 from twilio.base.page import Page
-from twilio.rest.api.v2010.account.calls import CallList
+from twilio.rest.api.v2010.account.call import CallList
 
 
 class AccountList(ListResource):
