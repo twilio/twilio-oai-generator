1c3e3ddde59f52a38ea24ca450097ea238d8d504
shrutiburman <87537688+shrutiburman@users.noreply.github.com>
2023-02-28 22:22:24 +0530

    chore: ruby integration tests (#441)
    <!--
    We appreciate the effort for this pull request but before that please
    make sure you read the contribution guidelines, then fill out the blanks
    below.
    
    Please format the PR title appropriately based on the type of change:
      <type>[!]: <description>
    Where <type> is one of: docs, chore, feat, fix, test, misc.
    Add a '!' after the type for breaking changes (e.g. feat!: new breaking
    feature).
    
    **All third-party contributors acknowledge that any contributions they
    provide will be made under the same open-source license that the
    open-source project is provided under.**
    
    Please enter each Issue number you are resolving in your PR after one of
    the following words [Fixes, Closes, Resolves]. This will auto-link these
    issues and close them when this PR is merged!
    e.g.
    Fixes #1
    Closes #2
    -->
    
    # Fixes [DII-777](https://issues.corp.twilio.com/browse/DII-777)
    
    This PR: 
    
    Adds integration tests for twilio-ruby
    
    ### Checklist
    - [x] I acknowledge that all my contributions will be made under the
    project's license
    - [x] Run `make test-docker`
    - [ ] Verify affected language:
    - [ ] Generate [twilio-go](https://github.com/twilio/twilio-go) from our
    [OpenAPI specification](https://github.com/twilio/twilio-oai) using the
    [build_twilio_go.py](./examples/build_twilio_go.py) using `python
    examples/build_twilio_go.py path/to/twilio-oai/spec/yaml
    path/to/twilio-go` and inspect the diff
        - [ ] Run `make test` in `twilio-go`
        - [ ] Create a pull request in `twilio-go`
        - [ ] Provide a link below to the pull request
    - [ ] I have made a material change to the repo (functionality, testing,
    spelling, grammar)
    - [ ] I have read the [Contribution
    Guidelines](https://github.com/twilio/twilio-oai-generator/blob/main/CONTRIBUTING.md)
    and my PR follows them
    - [ ] I have titled the PR appropriately
    - [ ] I have updated my branch with the main branch
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have added the necessary documentation about the functionality
    in the appropriate .md file
    - [ ] I have added inline documentation to the code I modified
    
    If you have questions, please create a GitHub Issue in this repository.

---
+++ examples/ruby/spec/integration/twilio_api_v2010_spec.rb
@@ -0,0 +1,65 @@
+describe 'Integration Test: api_v2010' do
+  before(:each) do
+    @account_sid = 'AC12345678123456781234567812345678'
+    @auth_token = 'CR12345678123456781234567812345678'
+  end
+
+  it "can create account" do
+    @holodeck.mock(Twilio::Response.new(200, '
+            {
+                    "sid": "AC12345678123456781234567812345678"
+            }'))
+    createdAccount = @client.api.v2010.accounts.create()
+    expect(createdAccount).to_not eq(nil)
+    expect(createdAccount.sid).to eq('AC12345678123456781234567812345678')
+  end
+
+  it "can fetch call" do
+    @holodeck.mock(Twilio::Response.new(200, '
+                {
+                        "sid": "AC12345678123456781234567812345678"
+                }'))
+    fetchedCall = @client.api.v2010.accounts(@account_sid).calls(123).fetch()
+    expect(fetchedCall).to_not eq(nil)
+    expect(fetchedCall.sid).to eq('AC12345678123456781234567812345678')
+  end
+
+  it "can create call" do
+    @holodeck.mock(Twilio::Response.new(
+                     201,
+                     %q[
+        {
+            "account_sid": "AC12345678123456781234567812345678",
+            "from": "+14158675308",
+            "from_formatted": "(415) 867-5308",
+            "group_sid": null,
+            "parent_call_sid": null,
+            "phone_number_sid": "PNaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        }
+        ]
+                   ))
+
+    createdCall = @client.api.v2010.accounts('AC12345678123456781234567812345678') \
+                         .calls.create(required_string_property: 'GET')
+
+    expect(createdCall).to_not eq(nil)
+  end
+
+  it "can update account" do
+    @holodeck.mock(Twilio::Response.new(200, '{
+              "account_sid": "AC12345678123456781234567812345678",
+              "sid": "ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+              "test_string": "Test String"
+             }'))
+
+    result = @client.api.v2010.accounts(@account_sid).update()
+    expect(result).to_not eq(nil)
+    expect(result.test_string).to eq("Test String")
+  end
+
+  it "can delete call" do
+    @holodeck.mock(Twilio::Response.new(204, nil))
+    deleteCall = @client.api.v2010.account.calls(123).delete()
+    expect(deleteCall).to eq(true)
+  end
+end
