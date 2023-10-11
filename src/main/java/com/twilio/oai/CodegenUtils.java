19b04b9e4a75d24e8c8ceedc4f8ffca02368cb55
kridai <kgoswami@twilio.com>
2023-10-11 12:26:08 +0530

    fix: restore codegenUtil

--- src/main/java/com/twilio/oai/CodegenUtils.java
+++ src/main/java/com/twilio/oai/CodegenUtils.java
@@ -3,9 +3,6 @@ package com.twilio.oai;
 import org.openapitools.codegen.CodegenParameter;
 import org.openapitools.codegen.CodegenProperty;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 public  class CodegenUtils {
 
     public static boolean isPropertySchemaEnum(CodegenProperty codegenProperty) {
@@ -20,29 +17,6 @@ public  class CodegenUtils {
     }
 
     public static boolean isParameterSchemaEnum(CodegenParameter codegenParameter) {
-        if (codegenParameter.isEnum) {
-            return true;
-        }
-        boolean enumValues = codegenParameter.allowableValues != null &&
-                codegenParameter.allowableValues.containsKey(TwilioJavaGenerator.VALUES);
-        boolean listEnumValues = codegenParameter.items != null && (codegenParameter.items.allowableValues != null
-                && codegenParameter.items.allowableValues.containsKey(TwilioJavaGenerator.VALUES));
-        return enumValues || listEnumValues;
-    }
-
-    // TODO: Refactor java code.
-    public static boolean isPropertySchemaEnumJava(CodegenProperty codegenProperty) {
-        if(codegenProperty.isEnum) {
-            return false;
-        }
-        boolean enumValues = codegenProperty.allowableValues != null &&
-                codegenProperty.allowableValues.containsKey(TwilioJavaGenerator.VALUES);
-        boolean listEnumValues = codegenProperty.items != null && (codegenProperty.items.allowableValues != null
-                && codegenProperty.items.allowableValues.containsKey(TwilioJavaGenerator.VALUES));
-        return enumValues || listEnumValues;
-    }
-
-    public static boolean isParameterSchemaEnumJava(CodegenParameter codegenParameter) {
         if(codegenParameter.isEnum) {
             return true;
         }
@@ -74,14 +48,5 @@ public  class CodegenUtils {
         boolean listEnumValues = codegenParameter.items != null && (codegenParameter.items.allowableValues != null
                 && codegenParameter.items.allowableValues.containsKey(TwilioJavaGenerator.VALUES));
         return enumValues || listEnumValues;
-    }
-
-    public static void mergeVendorExtensionProperty(Map<String, Object> vendorExtensions, LinkedHashMap value, String field ) {
-        if (value != null && !value.isEmpty()) {
-            if( vendorExtensions.containsKey(field))
-                ((LinkedHashMap)vendorExtensions.get(field)).putAll(value);
-            else
-                vendorExtensions.put(field, value);
-        }
     }
 }
