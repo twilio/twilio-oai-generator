35c9da510fd48403ab45f7507a883156abb63ad7
AsabuHere <asabu@twilio.com>
2023-10-17 14:35:16 +0530

    Adding modified version of generated files

--- examples/go/go-client/helper/rest/api/v2010/accounts_calls.go
+++ examples/go/go-client/helper/rest/api/v2010/accounts_calls.go
@@ -18,154 +18,161 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/url"
-	"strings"
+
+    "github.com/twilio/twilio-go/client"
 )
+
 
 // Optional parameters for the method 'CreateCall'
 type CreateCallParams struct {
-	//
-	PathAccountSid *string `json:"PathAccountSid,omitempty"`
-	//
-	RequiredStringProperty *string `json:"RequiredStringProperty,omitempty"`
-	//
-	TestArrayOfStrings *[]string `json:"TestArrayOfStrings,omitempty"`
-	//
-	TestArrayOfUri *[]string `json:"TestArrayOfUri,omitempty"`
-	// The HTTP method that we should use to request the `TestArrayOfUri`.
-	TestMethod *string `json:"TestMethod,omitempty"`
+    // 
+    PathAccountSid *string `json:"PathAccountSid,omitempty"`
+    // 
+    RequiredStringProperty *string `json:"RequiredStringProperty,omitempty"`
+    // 
+    TestArrayOfStrings *[]string `json:"TestArrayOfStrings,omitempty"`
+    // 
+    TestArrayOfUri *[]string `json:"TestArrayOfUri,omitempty"`
+    // The HTTP method that we should use to request the `TestArrayOfUri`.
+    TestMethod *string `json:"TestMethod,omitempty"`
 }
 
-func (params *CreateCallParams) SetPathAccountSid(PathAccountSid string) *CreateCallParams {
-	params.PathAccountSid = &PathAccountSid
-	return params
+func (params *CreateCallParams) SetPathAccountSid(PathAccountSid string) (*CreateCallParams){
+    params.PathAccountSid = &PathAccountSid
+    return params
 }
-func (params *CreateCallParams) SetRequiredStringProperty(RequiredStringProperty string) *CreateCallParams {
-	params.RequiredStringProperty = &RequiredStringProperty
-	return params
+func (params *CreateCallParams) SetRequiredStringProperty(RequiredStringProperty string) (*CreateCallParams){
+    params.RequiredStringProperty = &RequiredStringProperty
+    return params
 }
-func (params *CreateCallParams) SetTestArrayOfStrings(TestArrayOfStrings []string) *CreateCallParams {
-	params.TestArrayOfStrings = &TestArrayOfStrings
-	return params
+func (params *CreateCallParams) SetTestArrayOfStrings(TestArrayOfStrings []string) (*CreateCallParams){
+    params.TestArrayOfStrings = &TestArrayOfStrings
+    return params
 }
-func (params *CreateCallParams) SetTestArrayOfUri(TestArrayOfUri []string) *CreateCallParams {
-	params.TestArrayOfUri = &TestArrayOfUri
-	return params
+func (params *CreateCallParams) SetTestArrayOfUri(TestArrayOfUri []string) (*CreateCallParams){
+    params.TestArrayOfUri = &TestArrayOfUri
+    return params
 }
-func (params *CreateCallParams) SetTestMethod(TestMethod string) *CreateCallParams {
-	params.TestMethod = &TestMethod
-	return params
+func (params *CreateCallParams) SetTestMethod(TestMethod string) (*CreateCallParams){
+    params.TestMethod = &TestMethod
+    return params
 }
 
 func (c *ApiService) CreateCall(params *CreateCallParams) (*TestResponseObject, error) {
-	path := "/2010-04-01/Accounts/{AccountSid}/Calls.json"
-	if params != nil && params.PathAccountSid != nil {
-		path = strings.Replace(path, "{"+"AccountSid"+"}", *params.PathAccountSid, -1)
-	} else {
-		path = strings.Replace(path, "{"+"AccountSid"+"}", c.requestHandler.Client.AccountSid(), -1)
-	}
+    path := "/2010-04-01/Accounts/{AccountSid}/Calls.json"
+    if params != nil && params.PathAccountSid != nil {
+    path = strings.Replace(path, "{"+"AccountSid"+"}", *params.PathAccountSid, -1)
+} else {
+    path = strings.Replace(path, "{"+"AccountSid"+"}", c.requestHandler.Client.AccountSid(), -1)
+}
 
-	data := url.Values{}
-	headers := make(map[string]interface{})
+    data := url.Values{}
+    headers := make(map[string]interface{})
+if params != nil && params.RequiredStringProperty != nil {
+    data.Set("RequiredStringProperty", *params.RequiredStringProperty)
+}
+if params != nil && params.TestArrayOfStrings != nil {
+    for _, item  := range *params.TestArrayOfStrings {
+        data.Add("TestArrayOfStrings", item)
+    }
+}
+if params != nil && params.TestArrayOfUri != nil {
+    for _, item  := range *params.TestArrayOfUri {
+        data.Add("TestArrayOfUri", item)
+    }
+}
+if params != nil && params.TestMethod != nil {
+    data.Set("TestMethod", *params.TestMethod)
+}
 
-	if params != nil && params.RequiredStringProperty != nil {
-		data.Set("RequiredStringProperty", *params.RequiredStringProperty)
-	}
-	if params != nil && params.TestArrayOfStrings != nil {
-		for _, item := range *params.TestArrayOfStrings {
-			data.Add("TestArrayOfStrings", item)
-		}
-	}
-	if params != nil && params.TestArrayOfUri != nil {
-		for _, item := range *params.TestArrayOfUri {
-			data.Add("TestArrayOfUri", item)
-		}
-	}
-	if params != nil && params.TestMethod != nil {
-		data.Set("TestMethod", *params.TestMethod)
-	}
 
-	resp, err := c.requestHandler.Post(c.baseURL+path, data, headers)
-	if err != nil {
-		return nil, err
-	}
 
-	defer resp.Body.Close()
+    resp, err := c.requestHandler.Post(c.baseURL+path, data, headers)
+    if err != nil {
+        return nil, err
+    }
 
-	ps := &TestResponseObject{}
-	if err := json.NewDecoder(resp.Body).Decode(ps); err != nil {
-		return nil, err
-	}
+    defer resp.Body.Close()
 
-	return ps, err
+    ps := &TestResponseObject{}
+    if err := json.NewDecoder(resp.Body).Decode(ps); err != nil {
+        return nil, err
+    }
+
+    return ps, err
 }
 
 // Optional parameters for the method 'DeleteCall'
 type DeleteCallParams struct {
-	//
-	PathAccountSid *string `json:"PathAccountSid,omitempty"`
+    // 
+    PathAccountSid *string `json:"PathAccountSid,omitempty"`
 }
 
-func (params *DeleteCallParams) SetPathAccountSid(PathAccountSid string) *DeleteCallParams {
-	params.PathAccountSid = &PathAccountSid
-	return params
+func (params *DeleteCallParams) SetPathAccountSid(PathAccountSid string) (*DeleteCallParams){
+    params.PathAccountSid = &PathAccountSid
+    return params
 }
 
-func (c *ApiService) DeleteCall(TestInteger int, params *DeleteCallParams) error {
-	path := "/2010-04-01/Accounts/{AccountSid}/Calls/{TestInteger}.json"
-	if params != nil && params.PathAccountSid != nil {
-		path = strings.Replace(path, "{"+"AccountSid"+"}", *params.PathAccountSid, -1)
-	} else {
-		path = strings.Replace(path, "{"+"AccountSid"+"}", c.requestHandler.Client.AccountSid(), -1)
-	}
-	path = strings.Replace(path, "{"+"TestInteger"+"}", fmt.Sprint(TestInteger), -1)
+func (c *ApiService) DeleteCall(TestInteger int, params *DeleteCallParams) (error) {
+    path := "/2010-04-01/Accounts/{AccountSid}/Calls/{TestInteger}.json"
+    if params != nil && params.PathAccountSid != nil {
+    path = strings.Replace(path, "{"+"AccountSid"+"}", *params.PathAccountSid, -1)
+} else {
+    path = strings.Replace(path, "{"+"AccountSid"+"}", c.requestHandler.Client.AccountSid(), -1)
+}
+    path = strings.Replace(path, "{"+"TestInteger"+"}", fmt.Sprint(TestInteger), -1)
 
-	data := url.Values{}
-	headers := make(map[string]interface{})
+    data := url.Values{}
+    headers := make(map[string]interface{})
 
-	resp, err := c.requestHandler.Delete(c.baseURL+path, data, headers)
-	if err != nil {
-		return err
-	}
 
-	defer resp.Body.Close()
 
-	return nil
+    resp, err := c.requestHandler.Delete(c.baseURL+path, data, headers)
+    if err != nil {
+        return err
+    }
+
+    defer resp.Body.Close()
+
+    return nil
 }
 
 // Optional parameters for the method 'FetchCall'
 type FetchCallParams struct {
-	//
-	PathAccountSid *string `json:"PathAccountSid,omitempty"`
+    // 
+    PathAccountSid *string `json:"PathAccountSid,omitempty"`
 }
 
-func (params *FetchCallParams) SetPathAccountSid(PathAccountSid string) *FetchCallParams {
-	params.PathAccountSid = &PathAccountSid
-	return params
+func (params *FetchCallParams) SetPathAccountSid(PathAccountSid string) (*FetchCallParams){
+    params.PathAccountSid = &PathAccountSid
+    return params
 }
 
 func (c *ApiService) FetchCall(TestInteger int, params *FetchCallParams) (*TestResponseObject, error) {
-	path := "/2010-04-01/Accounts/{AccountSid}/Calls/{TestInteger}.json"
-	if params != nil && params.PathAccountSid != nil {
-		path = strings.Replace(path, "{"+"AccountSid"+"}", *params.PathAccountSid, -1)
-	} else {
-		path = strings.Replace(path, "{"+"AccountSid"+"}", c.requestHandler.Client.AccountSid(), -1)
-	}
-	path = strings.Replace(path, "{"+"TestInteger"+"}", fmt.Sprint(TestInteger), -1)
+    path := "/2010-04-01/Accounts/{AccountSid}/Calls/{TestInteger}.json"
+    if params != nil && params.PathAccountSid != nil {
+    path = strings.Replace(path, "{"+"AccountSid"+"}", *params.PathAccountSid, -1)
+} else {
+    path = strings.Replace(path, "{"+"AccountSid"+"}", c.requestHandler.Client.AccountSid(), -1)
+}
+    path = strings.Replace(path, "{"+"TestInteger"+"}", fmt.Sprint(TestInteger), -1)
 
-	data := url.Values{}
-	headers := make(map[string]interface{})
+    data := url.Values{}
+    headers := make(map[string]interface{})
 
-	resp, err := c.requestHandler.Get(c.baseURL+path, data, headers)
-	if err != nil {
-		return nil, err
-	}
 
-	defer resp.Body.Close()
 
-	ps := &TestResponseObject{}
-	if err := json.NewDecoder(resp.Body).Decode(ps); err != nil {
-		return nil, err
-	}
+    resp, err := c.requestHandler.Get(c.baseURL+path, data, headers)
+    if err != nil {
+        return nil, err
+    }
 
-	return ps, err
+    defer resp.Body.Close()
+
+    ps := &TestResponseObject{}
+    if err := json.NewDecoder(resp.Body).Decode(ps); err != nil {
+        return nil, err
+    }
+
+    return ps, err
 }
