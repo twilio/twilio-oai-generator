d6ec41f0efe73e47c936bb1c3b8ff6397c4d3c0d
manisha1997 <singhmanisha.250025@gmail.com>
2024-01-11 15:30:53 +0530

    feat: json-ingress for Ruby

--- examples/ruby/lib/twilio-ruby/rest/versionless/deployed_devices.rb
+++ examples/ruby/lib/twilio-ruby/rest/versionless/deployed_devices.rb
@@ -13,37 +13,39 @@
 #
 
 module Twilio
-    module REST
-        class Versionless
-            class DeployedDevices < Version
-                ##
-                # Initialize the DeployedDevices version of Versionless
-                def initialize(domain)
-                    super
-                    @version = 'DeployedDevices'
-                    @fleets = nil
-                end
-
-                ##
-                # @param [String] sid 
-                # @return [Twilio::REST::Versionless::DeployedDevices::FleetContext] if sid was passed.
-                # @return [Twilio::REST::Versionless::DeployedDevices::FleetList]
-                def fleets(sid=:unset)
-                    if sid.nil?
-                        raise ArgumentError, 'sid cannot be nil'
-                    end
-                    if sid == :unset
-                        @fleets ||= FleetList.new self
-                    else
-                        FleetContext.new(self, sid)
-                    end
-                end
-                ##
-                # Provide a user friendly representation
-                def to_s
-                    '<Twilio::REST::Versionless::DeployedDevices>';
-                end
-            end
+  module REST
+    class Versionless
+      class DeployedDevices < Version
+        ##
+        # Initialize the DeployedDevices version of Versionless
+        def initialize(domain)
+          super
+          @version = 'DeployedDevices'
+          @fleets = nil
         end
+
+        ##
+        # @param [String] sid
+        # @return [Twilio::REST::Versionless::DeployedDevices::FleetContext] if sid was passed.
+        # @return [Twilio::REST::Versionless::DeployedDevices::FleetList]
+        def fleets(sid = :unset)
+          if sid.nil?
+            raise ArgumentError, 'sid cannot be nil'
+          end
+
+          if sid == :unset
+            @fleets ||= FleetList.new self
+          else
+            FleetContext.new(self, sid)
+          end
+        end
+
+        ##
+        # Provide a user friendly representation
+        def to_s
+          '<Twilio::REST::Versionless::DeployedDevices>';
+        end
+      end
     end
+  end
 end
