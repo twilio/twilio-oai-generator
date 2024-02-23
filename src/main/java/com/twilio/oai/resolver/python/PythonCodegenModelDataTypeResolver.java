8b86447b36f7f165b4e66b5bdfb3897016a822e5
Shubham <stiwari@twilio.com>
2024-02-23 19:56:22 +0530

    chore: resolved enum values without "Enum" in name (#556)
    <!--
    We appreciate the effort for this pull request but before that please
    make sure you read the contribution guidelines, then fill out the blanks
    below.
    
    Please format the PR title appropriately based on the type of change:
      <type>[!]: <description>
    Where <type> is one of: docs, chore, feat, fix, test, misc.
    Add a '!' after the type for breaking changes (e.g. feat!: new breaking
    feature).
    
    **All third-party contributors acknowledge that any contributions they
    provide will be made under the same open-source license that the
    open-source project is provided under.**
    
    Please enter each Issue number you are resolving in your PR after one of
    the following words [Fixes, Closes, Resolves]. This will auto-link these
    issues and close them when this PR is merged!
    e.g.
    Fixes #1
    Closes #2
    -->
    
    # Fixes #
    
    There were some Enum objects that did not have 'enum' word in their
    datatype or anywhere else. See
    [here](https://github.com/twilio/twilio-oai/blob/2.0.0-rc-safe/spec/yaml/twilio_content_sdk.yaml#L75).
    But they also need to be resolved the same way. So added a check for
    that.
    
    ### Checklist
    - [x] I acknowledge that all my contributions will be made under the
    project's license
    - [x] Run `make test-docker`
    - [x] Verify affected language:
    - [ ] Generate [twilio-go](https://github.com/twilio/twilio-go) from our
    [OpenAPI specification](https://github.com/twilio/twilio-oai) using the
    [build_twilio_go.py](./examples/build_twilio_go.py) using `python
    examples/build_twilio_go.py path/to/twilio-oai/spec/yaml
    path/to/twilio-go` and inspect the diff
        - [ ] Run `make test` in `twilio-go`
        - [ ] Create a pull request in `twilio-go`
        - [ ] Provide a link below to the pull request
    - [x] I have made a material change to the repo (functionality, testing,
    spelling, grammar)
    - [x] I have read the [Contribution
    Guidelines](https://github.com/twilio/twilio-oai-generator/blob/main/CONTRIBUTING.md)
    and my PR follows them
    - [x] I have titled the PR appropriately
    - [x] I have updated my branch with the main branch
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have added the necessary documentation about the functionality
    in the appropriate .md file
    - [ ] I have added inline documentation to the code I modified
    
    If you have questions, please create a GitHub Issue in this repository.

--- src/main/java/com/twilio/oai/resolver/python/PythonCodegenModelDataTypeResolver.java
+++ src/main/java/com/twilio/oai/resolver/python/PythonCodegenModelDataTypeResolver.java
@@ -75,6 +75,12 @@ public class PythonCodegenModelDataTypeResolver extends CodegenModelDataTypeReso
         } else {
             property.vendorExtensions.put("json-name", property.name);
         }
+        if(CodegenUtils.isPropertySchemaEnum(property)
+            && !property.dataType.contains(ApplicationConstants.ENUM)
+            && !property.dataType.contains("Instance.")) {
+                property.baseType = ApplicationConstants.ENUM + property.baseType;
+                property.dataType = ApplicationConstants.ENUM + property.dataType;
+        }
         updateDataType(property.baseType, property.dataType, apiResourceBuilder, (dataTypeWithEnum, dataType) -> {
             property.datatypeWithEnum = dataTypeWithEnum;
             property.dataType = dataType;
