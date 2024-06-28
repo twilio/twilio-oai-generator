91fa4501b30162320d189c8ff50289208a6389b6
AsabuHere <asabu@twilio.com>
2024-06-28 11:27:37 +0530

    test fix

--- examples/java/src/main/java/com/twilio/Domains.java
+++ examples/java/src/main/java/com/twilio/Domains.java
@@ -4,8 +4,7 @@ public enum Domains {
     API("api"),
     FLEXAPI("flex-api"),
     VERSIONLESS("versionless"),
-    MESSAGING("messaging");
-
+    MESSAGING("messaging"),
     PREVIEWIAM("preview-iam");
 
     private final String value;
