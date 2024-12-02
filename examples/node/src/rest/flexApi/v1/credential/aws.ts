fb855a399ede41cc09a41ad06ff1c8168ded2feb
Shubham Tiwari <stiwari@twilio.com>
2024-12-02 19:03:53 +0530

    chore: setting header correctly

--- examples/node/src/rest/flexApi/v1/credential/aws.ts
+++ examples/node/src/rest/flexApi/v1/credential/aws.ts
@@ -152,11 +152,14 @@ export class AwsContextImpl implements AwsContext {
   remove(
     callback?: (error: Error | null, item?: boolean) => any,
   ): Promise<boolean> {
+    const headers: any = {};
+
     const instance = this;
     let operationVersion = instance._version,
       operationPromise = operationVersion.remove({
         uri: instance._uri,
         method: "delete",
+        headers,
       });
 
     operationPromise = instance._version.setPromiseCallback(
@@ -169,11 +172,15 @@ export class AwsContextImpl implements AwsContext {
   fetch(
     callback?: (error: Error | null, item?: AwsInstance) => any,
   ): Promise<AwsInstance> {
+    const headers: any = {};
+    headers["Accept"] = "application/json";
+
     const instance = this;
     let operationVersion = instance._version,
       operationPromise = operationVersion.fetch({
         uri: instance._uri,
         method: "get",
+        headers,
       });
 
     operationPromise = operationPromise.then(
@@ -210,6 +217,7 @@ export class AwsContextImpl implements AwsContext {
 
     const headers: any = {};
     headers["Content-Type"] = "application/x-www-form-urlencoded";
+    headers["Accept"] = "application/json";
 
     const instance = this;
     let operationVersion = instance._version,
@@ -485,6 +493,7 @@ export function AwsListInstance(version: V1): AwsListInstance {
     if (params.pageToken !== undefined) data["PageToken"] = params.pageToken;
 
     const headers: any = {};
+    headers["Accept"] = "application/json";
 
     let operationVersion = version,
       operationPromise = operationVersion.page({
