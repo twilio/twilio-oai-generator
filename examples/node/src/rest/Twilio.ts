8c1b3c40085922e2e36965ed3c14a6472eb33009
Shubham Tiwari <stiwari@twilio.com>
2024-12-12 13:26:25 +0530

    chore: add preview-iam examples to node

--- examples/node/src/rest/Twilio.ts
+++ examples/node/src/rest/Twilio.ts
@@ -2,6 +2,7 @@ import { Client, ClientOpts } from "../base/BaseTwilio";
 import ApiBase from "./ApiBase";
 import FlexApiBase from "./FlexApiBase";
 import VersionlessBase from "./VersionlessBase";
+import PreviewIamBase = require("./PreviewIamBase");
 
 class Twilio extends Client {
   constructor(username?: string, password?: string, opts?: ClientOpts) {
@@ -18,6 +19,10 @@ class Twilio extends Client {
 
   get versionless(): VersionlessBase {
     return new VersionlessBase(this);
+  }
+
+  get previewIam(): PreviewIamBase {
+    return new PreviewIamBase(this);
   }
 }
 
