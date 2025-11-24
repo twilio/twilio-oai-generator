2de008e3719f0788ae259a8bc79beb8f0ca2025f
Shubham Tiwari <stiwari@twilio.com>
2025-11-24 12:08:45 +0530

    chore: add iApiV1 flag to ApiResources

--- src/main/java/com/twilio/oai/api/PhpApiResources.java
+++ src/main/java/com/twilio/oai/api/PhpApiResources.java
@@ -8,13 +8,11 @@ import java.util.List;
 public class PhpApiResources extends ApiResources{
     private String apiListPath;
     private String apiContextPath;
-    private Boolean isApiV1 = null; // true or NUll
 
     public PhpApiResources(PhpApiResourceBuilder apiResourceBuilder) {
         super(apiResourceBuilder);
 
         apiListPath = apiResourceBuilder.apiListPath;
         apiContextPath = apiResourceBuilder.apiContextPath;
-        if (ResourceCacheContext.get().isV1()) isApiV1 = true;
     }
 }
