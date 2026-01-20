f75a414d25172742dd24772784d25a01fe256d4a
Manisha Singh <singhmanisha.250025@gmail.com>
2026-01-20 16:10:04 +0530

    chore: ruby bug for api standards (#758)
    bugs for api standards:
    
    1. when limits were not in multiples of page_size, it was showing less
    records than limits.
    2. some language specific variable conventions

--- examples/ruby/lib/twilio-ruby/rest/versionless/deployed_devices/fleet.rb
+++ examples/ruby/lib/twilio-ruby/rest/versionless/deployed_devices/fleet.rb
@@ -228,13 +228,13 @@ module Twilio
             @fleet_page = []
             @limit = limit
             key = get_key(response.body)
-            number_of_records = response.body[key].size
-            while (limit != :unset && number_of_records <= limit)
-              @fleet_page << FleetListResponse.new(version, @payload, key)
+            records = 0
+            while (limit != :unset && records < limit)
+              @fleet_page << FleetListResponse.new(version, @payload, key, limit - records)
               @payload = self.next_page
               break unless @payload
 
-              number_of_records += @payload.body[key].size
+              records += @payload.body[key].size
             end
             # Path Solution
             @solution = solution
@@ -255,8 +255,12 @@ module Twilio
           # @param [Array<FleetInstance>] instance
           # @param [Hash{String => Object}] headers
           # @param [Integer] status_code
-          def initialize(version, payload, key)
-            @fleet = payload.body[key].map do |data|
+          def initialize(version, payload, key, limit = :unset)
+            data_list = payload.body[key]
+            if limit != :unset
+              data_list = data_list[0, limit]
+            end
+            @fleet = data_list.map do |data|
               FleetInstance.new(version, data)
             end
             @headers = payload.headers
