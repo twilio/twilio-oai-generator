38ccb0b5f2f3985d673a94ba46ac8791385d90b7
kridai <kgoswami@twilio.com>
2026-03-24 15:21:36 +0530

    chore: added fix for snake case nested attributes and import keywork … (#779)
    Added fix for snake case nested attributes and import keywork error
    added the test generation for memory/v1 here to preview
    https://github.com/twilio/twilio-python/pull/917
    
    snake case 
    
    https://github.com/twilio/twilio-python/pull/917/changes#diff-777836e65412325fc37488cd2666c3c5a395f055d5dddee403544a8386dfebf7R48
    
    import keyword generation :
    https://github.com/twilio/twilio-python/pull/917/changes#diff-116f38979d5ea6f70e7b59044645b2666073bb9259c94a51492bff8ca69f5574
    
    <!--
    We appreciate the effort for this pull request but before that please
    make sure you read the contribution guidelines, then fill out the blanks
    below.
    
    Please format the PR title appropriately based on the type of change:
      <type>[!]: <description>
    Where <type> is one of: docs, chore, feat, fix, test, misc.
    Add a '!' after the type for breaking changes (e.g. feat!: new breaking
    feature).
    
    **All third-party contributors acknowledge that any contributions they
    provide will be made under the same open-source license that the
    open-source project is provided under.**
    
    Please enter each Issue number you are resolving in your PR after one of
    the following words [Fixes, Closes, Resolves]. This will auto-link these
    issues and close them when this PR is merged!
    e.g.
    Fixes #1
    Closes #2
    -->
    
    # Fixes #
    
    A short description of what this PR does.
    
    ### Checklist
    - [x] I acknowledge that all my contributions will be made under the
    project's license
    - [ ] Run `make test-docker`
    - [ ] Verify affected language according to the code change:
    - [ ] Generate [twilio-java](https://github.com/twilio/twilio-java) from
    our [OpenAPI specification](https://github.com/twilio/twilio-oai) using
    the [scripts/build_twilio_library.py](./scripts/build_twilio_library.py)
    using `python scripts/build_twilio_library.py
    path/to/twilio-oai/spec/yaml path/to/twilio-java -l java` and inspect
    the diff
        - [ ] Run `make test` in `twilio-java`
        - [ ] Create a pull request in `twilio-java`
    - [ ] Provide a link below to the pull request, this ensures that the
    generated code has been verified
    - [ ] I have made a material change to the repo (functionality, testing,
    spelling, grammar)
    - [ ] I have read the [Contribution
    Guidelines](https://github.com/twilio/twilio-oai-generator/blob/main/CONTRIBUTING.md)
    and my PR follows them
    - [ ] I have titled the PR appropriately
    - [ ] I have updated my branch with the main branch
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have added the necessary documentation about the functionality
    in the appropriate .md file
    - [ ] I have added inline documentation to the code I modified
    
    If you have questions, please create a GitHub Issue in this repository.

--- src/main/java/com/twilio/oai/resolver/python/PythonCaseResolver.java
+++ src/main/java/com/twilio/oai/resolver/python/PythonCaseResolver.java
@@ -4,7 +4,19 @@ import com.twilio.oai.resolver.CaseResolver;
 
 import org.openapitools.codegen.utils.StringUtils;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class PythonCaseResolver implements CaseResolver {
+    // Python reserved keywords that cannot be used as filenames
+    private static final Set<String> PYTHON_RESERVED_KEYWORDS = new HashSet<>(Arrays.asList(
+        "import", "from", "class", "def", "return", "if", "else", "elif", "for", "while",
+        "break", "continue", "pass", "try", "except", "finally", "raise", "with", "as",
+        "assert", "del", "global", "lambda", "yield", "None", "True", "False", "and",
+        "or", "not", "in", "is"
+    ));
+
     @Override //not sure
     public String productOperation(final String product) {
         return StringUtils.underscore(product);
@@ -12,11 +24,21 @@ public class PythonCaseResolver implements CaseResolver {
 
     @Override //flex_flow.py
     public String pathOperation(final String pathPart) {
-        return StringUtils.underscore(pathPart);
+        String result = StringUtils.underscore(pathPart);
+        // If the result is a Python reserved keyword, append underscore
+        if (PYTHON_RESERVED_KEYWORDS.contains(result.toLowerCase())) {
+            return result + "_";
+        }
+        return result;
     }
 
     @Override
     public String filenameOperation(final String filename) {
-        return pathOperation(filename);
+        String result = pathOperation(filename);
+        // If the filename (without extension) is a Python reserved keyword, append underscore
+        if (PYTHON_RESERVED_KEYWORDS.contains(result.toLowerCase())) {
+            return result + "_";
+        }
+        return result;
     }
 }
