d904ff9b429d964bcc6937734a8a23b71b81597c
manisha1997 <singhmanisha.250025@gmail.com>
2026-04-09 15:44:12 +0530

    feat: fix bugs for api standards

--- src/main/java/com/twilio/oai/java/cache/ResourceCache2.java
+++ src/main/java/com/twilio/oai/java/cache/ResourceCache2.java
@@ -12,7 +12,7 @@ import org.openapitools.codegen.CodegenProperty;
 import java.util.*;
 
 public class ResourceCache2 {
-    
+
     @Getter
     @Setter
     private String resourceName;
@@ -51,7 +51,7 @@ public class ResourceCache2 {
     @Getter
     @Setter
     private boolean isV1;
-    
+
     @Getter
     @Setter
     private HashMap<String, Object> additionalProperties = new HashMap<>();
@@ -85,6 +85,11 @@ public class ResourceCache2 {
         modelClassesForMustache.clear();
         enumsClassesForMustache.clear();
         response.clear();
+        responseCreate.clear();
+        responseUpdate.clear();
+        responseList.clear();
+        responseFetch.clear();
+        responsePatch.clear();
     }
 
     // No need to clear
